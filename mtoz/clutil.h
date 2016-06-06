86de4baa71e6a7a267a266e92ee8ec3410c013cc
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2016-06-06 05:06:33 +0000

    And again, Identity the hard way...

--- mtoz/clutil.h
+++ mtoz/clutil.h
@@ -16,35 +16,15 @@ using uint64 = uint64_t;
 
 #define CHECK_SUCCESS(e) \
   do { int ret = (e); if (ret != CL_SUCCESS) { \
-  fprintf(stderr, "Not successful with code %d.\n", ret); \
+      fprintf(stderr, "Not successful with code %d (%s).\n", ret, CL::ErrorString(ret)); \
   abort(); } } while (0)
 
 // Boilerplate. There should probably just be one of these per program.
 struct CL {
-  CL() {
-    cl_uint num_platforms;
-    cl_platform_id platform = nullptr;
-    CHECK(CL_SUCCESS == clGetPlatformIDs(0, nullptr, &num_platforms));
+  CL();
 
-    // Choose the first platform. XXX Tom: Look into this to ensure this is what we actually want.
-    if (num_platforms > 0) {
-      cl_platform_id *platforms = (cl_platform_id *)malloc(num_platforms * sizeof(cl_platform_id));
-      CHECK(CL_SUCCESS == clGetPlatformIDs(num_platforms, platforms, nullptr));
-      platform = platforms[0];
-      free(platforms);
-    }
-
-    // Get the GPU device.
-    CHECK(CL_SUCCESS == clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &num_devices));
-    CHECK(num_devices > 0);
-
-    devices = (cl_device_id *)malloc(num_devices * sizeof(cl_device_id));
-    CHECK(CL_SUCCESS == clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, num_devices, devices, nullptr));
-
-    context = clCreateContext(nullptr, 1, devices, nullptr, nullptr, nullptr);
-    queue = clCreateCommandQueue(context, devices[0], 0, nullptr);
-  }
-
+  static const char *ErrorString(cl_int err);
+  
   cl_command_queue NewCommandQueue(bool out_of_order = true) {
     return clCreateCommandQueue(context, devices[0], 
 				out_of_order ? CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE : 0,
@@ -52,28 +32,7 @@ struct CL {
   }
 
   pair<cl_program, cl_kernel> BuildOneKernel(const string &kernel_src,
-					     const string &function_name) {
-    Timer gpu_compile;
-    const char *sources[] = { kernel_src.c_str() };
-    size_t source_size[] = { kernel_src.size() };
-    cl_program program = clCreateProgramWithSource(context, 1, sources, source_size, nullptr);
-    if (CL_SUCCESS != clBuildProgram(program, 1, devices, nullptr, nullptr, nullptr)) {
-      size_t blsize;
-
-      CHECK(CL_SUCCESS == clGetProgramBuildInfo(program, devices[0], 
-						CL_PROGRAM_BUILD_LOG, 0, nullptr, &blsize));
-      char *build_log = (char *)malloc(blsize + 1);
-      CHECK(CL_SUCCESS == clGetProgramBuildInfo(program, devices[0], 
-						CL_PROGRAM_BUILD_LOG, blsize, build_log, nullptr));
-      build_log[blsize] = 0;
-      printf("Failed to compile:\n %s", build_log);
-      exit(-1);
-    }
-
-    cl_kernel kernel = clCreateKernel(program, function_name.c_str(), nullptr);
-    fprintf(stderr, "Compiled %s in %.1fms.\n", function_name.c_str(), gpu_compile.MS());
-    return make_pair(program, kernel);
-  }
+					     const string &function_name);
 
   ~CL() {
     printf("Destroying CL.\n");
