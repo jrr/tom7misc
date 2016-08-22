cca71832f595bdc7f18194057acc8697c37b24a6
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2016-08-22 22:54:00 +0000

    Recompress some large/miscellaneous PNGs with zopfli

---
+++ misc/zopinplace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# /bin/find . -name "*.png" | xargs -n 1 -P 8 zopinplace.sh
+
+if test "$#" -ne 1; then
+    echo "zopinplace.sh filename.png"
+    exit 1
+fi
+
+set -x
+TMP=`mktemp -p . -t zopfli_XXXXXXXX.png`
+echo "Optimizing $1 to ${TMP}..."
+nice -n 16 zopflipng.exe --iterations=500 --splitting=3 --filters=01234mepb --lossy_8bit --lossy_transparent "$1" "${TMP}" || exit -1
+rm -f "$1"
+mv "${TMP}" "$1"
