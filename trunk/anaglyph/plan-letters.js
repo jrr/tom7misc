3cfe28a8983268a4f6df44d5eb17a3f3ba7e1316
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2017-07-14 22:12:19 +0000

    Starting point for letter self-decomposition

---
+++ trunk/anaglyph/plan-letters.js
@@ -0,0 +1,14 @@
+let startword = 'pittsburgh';
+let endword = 'tight burps';
+let plan = [
+  {a:"p",h:-1,ss:0,sp:0,ds:9,dp:0},
+  {a:"i",h:0,ss:1,sp:0,ds:1,dp:0},
+  {a:"t",h:1,ss:2,sp:0,ds:0,dp:0},
+  {a:"t",h:-1,ss:3,sp:0,ds:4,dp:0},
+  {a:"s",h:-1,ss:4,sp:0,ds:10,dp:0},
+  {a:"b",h:-1,ss:5,sp:0,ds:6,dp:0},
+  {a:"u",h:-1,ss:6,sp:0,ds:7,dp:0},
+  {a:"r",h:-1,ss:7,sp:0,ds:8,dp:0},
+  {a:"g",h:1,ss:8,sp:0,ds:2,dp:0},
+  {a:"h",h:1,ss:9,sp:0,ds:3,dp:0}
+];
