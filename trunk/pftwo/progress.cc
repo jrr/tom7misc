893df075a5b8f73e09d543973f178a6e26914355
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-01-27 15:47:52 +0000

    Next experiment: Budget for max depth, tweak constants to prefer expanding good (early in heap) nodes, and to expand deeper (back to 50% chance of sticking with node). Icon for playback. Display deep trees better.

--- trunk/pftwo/progress.cc
+++ trunk/pftwo/progress.cc
@@ -59,14 +59,19 @@ struct Plots {
 // TODO HERE:
 static Plots OneMovie(const string &game,
 		      const string &moviename) {
-  vector<pair<uint8, uint8>> movie = SimpleFM2::ReadInputs2P(moviename);
+  vector<pair<int, string>> subs;
+  vector<pair<uint8, uint8>> movie =
+    SimpleFM2::ReadInputsEx(moviename, &subs);
   CHECK(!movie.empty()) << "Couldn't read movie: " << moviename; 
   unique_ptr<Emulator> emu{Emulator::Create(game)};
   CHECK(emu.get() != nullptr) << game;
 
   Plots ret;
   ret.moviename = moviename;
+
+  RE2 subtitle_re{"f +([0-9]+) +s +([0-9]+)"};
   
+  int subidx = 0;
   for (int frame = 0; frame < movie.size(); frame++) {
     uint8 p1, p2;
     std::tie(p1, p2) = movie[frame];
@@ -74,12 +79,23 @@ static Plots OneMovie(const string &game,
     vector<uint8> mem = emu->GetMemory();
     
     // Note: Contra-specific. Could come from .objectives file?
+    // XXX using full 256 bytes for these makes really big jumps...
     uint8 stage = mem[48];
     uint8 room = mem[100];
     uint8 scroll = mem[101];
     double pos = (int)scroll + 256 * (int)room + 65536 * (int)stage;
     ret.depth_pos.points.push_back({(double)frame, pos});
 
+    if (subidx < subs.size() && subs[subidx].first == frame) {
+      int64 nes_frames, secs;
+      if (RE2::PartialMatch(subs[subidx].second, subtitle_re,
+			    &nes_frames, &secs)) {
+	ret.mframes_pos.points.push_back({(double)nes_frames, pos});
+	ret.walltime_pos.points.push_back({(double)secs, pos});
+      }
+      subidx++;
+    }
+    
     if (frame % 10000 == 0) {
       Printf("%s %d/%d\n", moviename.c_str(), frame, (int)movie.size());
     }
@@ -119,22 +135,24 @@ static void DrawPlots(const vector<Plots> &plotses,
   }
 
   bounds.AddMarginFrac(0.05);
-  Bounds::Scaler scaler = bounds.Stretch(width, height).FlipY();
+  Bounds::Scaler scaler = bounds.Stretch(width, height); /* .FlipY(); */
   // Now output the lines
   int coloridx = 0;
   for (const Plots &plots : plotses) {
     const Series &series = get(plots);
-    const string &color = Colors()[coloridx];
-    fprintf(f, "<polyline stroke-linejoin=\"round\" "
-	    "fill=\"none\" stroke=\"%s\" stroke-opacity=\"0.75\" "
-	    "stroke-width=\"0.75\" points=\"",
-	    color.c_str());
-    for (const std::pair<double, double> pt : series.points) {
-      double x, y;
-      std::tie(x, y) = scaler.Scale(pt);
-      fprintf(f, "%s,%s ", Rtos(x).c_str(), Rtos(y).c_str());
+    if (!series.points.empty()) {
+      const string &color = Colors()[coloridx];
+      fprintf(f, "<polyline stroke-linejoin=\"round\" "
+	      "fill=\"none\" stroke=\"%s\" stroke-opacity=\"0.75\" "
+	      "stroke-width=\"0.75\" points=\"",
+	      color.c_str());
+      for (const std::pair<double, double> pt : series.points) {
+	double x, y;
+	std::tie(x, y) = scaler.Scale(pt);
+	fprintf(f, "%s,%s ", Rtos(x).c_str(), Rtos(y).c_str());
+      }
+      fprintf(f, "\"/>\n");
     }
-    fprintf(f, "\"/>\n");
     coloridx ++;
     coloridx %= Colors().size();
   }
@@ -143,12 +161,13 @@ static void DrawPlots(const vector<Plots> &plotses,
   coloridx = 0;
   for (const Plots &plots : plotses) {
     const Series &series = get(plots);
-    if (series.points.empty()) break;
-    const string &color = Colors()[coloridx];
-    double x, y;
-    std::tie(x, y) = scaler.Scale(series.points[series.points.size() - 1]);
-    fprintf(f, TextSVG::Text(x, y, "sans-serif", 12.0,
-			     {{color, plots.moviename}}).c_str());
+    if (!series.points.empty()) {
+      const string &color = Colors()[coloridx];
+      double x, y;
+      std::tie(x, y) = scaler.Scale(series.points[series.points.size() - 1]);
+      fprintf(f, TextSVG::Text(x, y, "sans-serif", 12.0,
+			       {{color, plots.moviename}}).c_str());
+    }
     coloridx ++;
     coloridx %= Colors().size();
   }
@@ -184,9 +203,14 @@ int main(int argc, char *argv[]) {
 				      }, 10);
 
   // Now collate and write plots.
-  // XXX compute bounds from data, and normalize data!
   DrawPlots(plotses, 1024, 1024, "depth-pos.svg",
 	    [](const Plots &p) { return p.depth_pos; });
-  
+
+  DrawPlots(plotses, 1024, 1024, "mframes-pos.svg",
+	    [](const Plots &p) { return p.mframes_pos; });
+
+  DrawPlots(plotses, 1024, 1024, "walltime-pos.svg",
+	    [](const Plots &p) { return p.walltime_pos; });
+
   return 0;
 }
