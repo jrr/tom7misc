6b76115c396297b3ceeabfb05a2a98fbaf02f14b
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-02-05 05:13:48 +0000

    Insert into database

--- trunk/tempo/tempo.cc
+++ trunk/tempo/tempo.cc
@@ -5,6 +5,9 @@
 #include <cstdint>
 #include <unordered_map>
 
+#include <mysql++.h>
+
+#include "../cc-lib/base/stringprintf.h"
 #include "../cc-lib/base/logging.h"
 #include "../cc-lib/util.h"
 
@@ -12,21 +15,7 @@ using namespace std;
 using uint8 = uint8_t;
 using uint16 = uint16_t;
 using uint32 = uint32_t;
-
-// Util::ReadFile doesn't work, probably because
-// fseek doesn't 
-static string ReadFile(const string &filename) {
-  FILE *f = fopen(filename.c_str(), "rb");
-  if (!f) return "";
-  string ret;
-  ret.reserve(128);
-  int c = 0;
-  while (EOF != (c = getc(f))) {
-    ret += (char)c;
-  }
-  fclose(f);
-  return ret;
-}
+using uint64 = uint64_t;
 
 struct OneWire {
   const string dir = "/sys/bus/w1/devices";
@@ -83,15 +72,75 @@ struct OneWire {
   std::unordered_map<string, Probe> probes;
 };
 
+// Manages database connection.
+struct Database {
+  using Connection = mysqlpp::Connection;
+  using Query = mysqlpp::Query;
+  using StoreQueryResult = mysqlpp::StoreQueryResult;
+  
+  map<string, string> config;
+  map<string, int> probes;
+  const string configfile = "database-config.txt";
+  const string database_name = "tempo";
+
+  Connection conn{false};
+
+  Database() {
+    config = Util::ReadFileToMap(configfile);
+    const string server = config["server"];
+    const string user = config["user"];
+    const string password = config["password"];
+    CHECK(!server.empty()) << "Specify in " << configfile;
+    CHECK(!user.empty()) << "Specify in " << configfile;
+    CHECK(!user.empty()) << "Specify in " << configfile;
+    CHECK(conn.connect(database_name.c_str(),
+		       server.c_str(),
+		       user.c_str(),
+		       password.c_str()));
+
+    // Just read all the probes into a local map.
+    Query q = conn.query(
+	"select id, code, description from probe order by id");
+    StoreQueryResult res = q.store();
+    CHECK(res) << "Probe setup query failed";
+    for (size_t i = 0; i < res.num_rows(); i++) {
+      const int id = res[i]["id"];
+      const char *code = res[i]["code"];
+      const char *desc = res[i]["description"];
+      probes[code] = id;
+      printf("%d. %s: %s\n", id, code, desc);
+    }
+  }
+
+  // code should be like "28-000009ffbb20"
+  void WriteTemp(const string &code, int microdegs_c) {
+    auto it = probes.find(code);
+    if (it == probes.end()) {
+      printf("Unknown probe %s!\n", code.c_str());
+      return;
+    }
+
+    const int id = it->second;
+    uint64 now = time(nullptr);
+    string qs = StringPrintf("insert into tempo.reading "
+			     "(timestamp, probeid, microdegsc) "
+			     "values (%llu, %d, %d)",
+			     now, id, microdegs_c);
+    Query q = conn.query(qs.c_str());
+    (void)q.store();
+  }
+};
 
 int main(int argc, char **argv) {
   OneWire onewire;
-
+  Database db;
+  
   for (;;) {
     for (auto &p : onewire.probes) {
       uint32 microdegs_c = 0;
       if (p.second.Temperature(&microdegs_c)) {
 	printf("%s: %u\n", p.first.c_str(), microdegs_c);
+	db.WriteTemp(p.first, microdegs_c);
       } else {
 	printf("%s: ERROR\n", p.first.c_str());
       }
