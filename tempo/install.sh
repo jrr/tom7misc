3327e9f34cd37a7f9f30f13828ccfe2b2ed378d6
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-03-24 15:16:18 +0000

    more startup config, plus record readings per second for benchmarking

--- tempo/install.sh
+++ tempo/install.sh
@@ -10,12 +10,21 @@ else
 	sudo sed --in-place 's/XKBLAYOUT="gb"/XKBLAYOUT="us"/g' /etc/default/keyboard
 fi
 
-# TODO: US KEYBOARD LAYOUT
+if grep FONTFACE=.TerminusBold. /etc/default/console-setup;
+then
+	echo Console font/size OK.
+else
+	echo Set to low-res fonts for TV use.
+	sudo sed --in-place 's/FONTFACE=.*/FONTFACE="TerminusBold"/g' /etc/default/console-setup
+	sudo sed --in-place 's/FONTSIZE=.*/FONTSIZE="14x28"/g' /etc/default/console-setup
+fi
+
+# TODO: Also modify /etc/default/locale ?
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install emacs24 subversion
-sudo apt-get -y install default-libmysqlclient-dev
-sudo apt-get -y install libmysql++-dev
+sudo apt-get -y install default-libmysqlclient-dev libmysql++-dev mysql-client
 
 # Really grab the whole thing? It takes kinda long...
 # XXX just svn up if it's already present.
@@ -23,5 +32,13 @@ echo yes | svn co svn+ssh://tom7@svn.code.sf.net/p/tom7misc/svn/trunk tom7misc
 cd tom7misc/tempo
 cp /boot/database-config.txt .
 make
-./tempo
+
+sudo echo "#!/bin/sh -e" > /etc/rc.local
+sudo echo "# Installed by tempo on $(date)" >> /etc/rc.local
+sudo echo "/home/pi/tom7misc/tempo/onstart.sh &" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
+sudo chmod a+rx /etc/rc.local
+chmod a+rx /home/pi/tom7misc/tempo/onstart.sh
+
+./tempo.exe
 
---
+++ tempo/onstart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/tom7misc/tempo
+./tempo.exe
+# TODO: On exit, retry. Periodically reboot.
