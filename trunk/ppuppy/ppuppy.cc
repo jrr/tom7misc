b6b6595511f723d2313f78e1d21f3a8f47e40f54
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-03-28 03:17:46 +0000

    Version that strobes

--- trunk/ppuppy/ppuppy.cc
+++ trunk/ppuppy/ppuppy.cc
@@ -7,10 +7,12 @@
 #include "bcm2835.h"
 // #include "util.h"
 #include "base/logging.h"
+#include "arcfour.h"
 // #include "base/stringprintf.h"
 
 using namespace std;
 using uint8 = uint8_t;
+using uint16 = uint16_t;
 using uint32 = uint32_t;
 
 int main(int argc, char **argv) {
@@ -24,6 +26,12 @@ int main(int argc, char **argv) {
   
   CHECK(bcm2835_init());
 
+  ArcFour rc("ppuppy");
+  uint8 mem[8192] = {};
+  for (int i = 0; i < 8192; i++) {
+    mem[i] = rc.Byte();
+  }
+  
   printf("START.\n");
   static constexpr uint8 PIN = 26;
   static constexpr uint8 PIN2 = 6;
@@ -35,16 +43,19 @@ int main(int argc, char **argv) {
     bcm2835_gpio_set_pud(p, BCM2835_GPIO_PUD_OFF);
   }
 
-  uint8 value = 0;
+  int index = 0;
+  uint16 value = 0;
   for (;;) {
     // bcm2835_delayMicroseconds(1000); // 1ms
     // bcm2835_gpio_write(PIN, value & 1);
-    /*
-    uint8 v = value & 1;
+
+    uint8 v = (value >> 14) & 1;
     bcm2835_gpio_write_mask(
 	(v << PIN) | (v << PIN2) | (v << PIN3) | (v << PIN4),
 	(1 << PIN) | (1 << PIN2) | (1 << PIN3) | (1 << PIN4));
-    */
+    value++;
+    
+    // uint8 value = mem[index];
     uint32 word = ((value & 1) << PIN) |
       (((value >> 1) & 1) << PIN2) |
       (((value >> 2) & 1) << PIN3) |
@@ -52,7 +63,8 @@ int main(int argc, char **argv) {
     bcm2835_gpio_write_mask(word,
 			    (1 << PIN) | (1 << PIN2) | (1 << PIN3) | (1 << PIN4));
     
-    value++;
+    // index++;
+    // index &= 8191;
   }
   
   CHECK(bcm2835_close());
