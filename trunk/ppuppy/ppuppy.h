f04e07873c0cdf7c0b2caa879644dba9384bc386
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-04-14 23:30:30 +0000

    45 minutes of soldering later...

--- trunk/ppuppy/ppuppy.h
+++ trunk/ppuppy/ppuppy.h
@@ -13,24 +13,25 @@ using int32 = int32_t;
 using int64 = int64_t;
 
 // input pins, as wired on red solder board
-static constexpr uint8 PIN_RD = 16;
+static constexpr uint8 PIN_RD = 12;
 static constexpr uint8 PIN_A0 = 14;
 static constexpr uint8 PIN_A1 = 15;
-static constexpr uint8 PIN_A2 = 18;
-static constexpr uint8 PIN_A3 = 23;
-static constexpr uint8 PIN_A4 = 24;
-static constexpr uint8 PIN_A5 = 25;
-static constexpr uint8 PIN_A6 = 8;
-static constexpr uint8 PIN_A7 = 7;
-static constexpr uint8 PIN_A8 = 12;
-static constexpr uint8 PIN_A9 = 20;
-static constexpr uint8 PIN_A13 = 21;
+static constexpr uint8 PIN_A2 = 16;
+static constexpr uint8 PIN_A3 = 17;
+static constexpr uint8 PIN_A4 = 18;
+static constexpr uint8 PIN_A5 = 19;
+static constexpr uint8 PIN_A6 = 20;
+static constexpr uint8 PIN_A7 = 21;
+static constexpr uint8 PIN_A8 = 22;
+static constexpr uint8 PIN_A9 = 23;
+static constexpr uint8 PIN_A13 = 26;
 
 // output pins, wired to bus transciever
 static constexpr uint8 POUT_D0 = 2;
 static constexpr uint8 POUT_D1 = 3;
 static constexpr uint8 POUT_D2 = 4;
 static constexpr uint8 POUT_D3 = 5;
+static constexpr uint8 POUT_D4 = 6;
 // Use 6 next, of course. But 7 is used on address side.
 
 inline uint32 Encode(uint8 byte) {
