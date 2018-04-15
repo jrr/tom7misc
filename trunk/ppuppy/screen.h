b8f2491efe4bfc4db59d73ed7448ba6a8ec1e3a4
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-04-15 16:00:08 +0000

    Clean up code a bit now.

--- trunk/ppuppy/screen.h
+++ trunk/ppuppy/screen.h
@@ -23,16 +23,4 @@ struct Screen {
   uint8 color_hi[NUM_SCANLINES * NUM_COLS];
 };
 
-// Same, but pre-encoded for the output pins.
-// PERF (?): Now encoding is just << 2, so probably can go
-// back to raw bytes.
-struct EncodedScreen {
-  // PERF: I can probably get away with uint16. Is it faster?
-  uint32 encoded_attr[NUM_SCANLINES * NUM_COLS];
-  uint32 encoded_color_lo[NUM_SCANLINES * NUM_COLS];
-  uint32 encoded_color_hi[NUM_SCANLINES * NUM_COLS];
-};
-
-void EncodeScreen(const Screen &screen, EncodedScreen *encoded);
-
 #endif
