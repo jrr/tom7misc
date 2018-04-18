ce6a5e7d936543d6cfd8329cc5403411ad848780
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-04-18 03:36:52 +0000

    Enhance makimage to allow for extracting actual CHR from test cart (but don't check in)

--- trunk/ppuppy/cart/makeimage.c
+++ trunk/ppuppy/cart/makeimage.c
@@ -8,21 +8,31 @@
 // files, so it's not going to work in the general case...
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
-    fprintf(stderr, "usage: makeimage.exe cart.nes cart.rom\n");
+  if (argc != 4) {
+    fprintf(stderr, "usage: makeimage.exe -prg|-chr cart.nes cart.rom\n");
     return -1;
   }
 
-  FILE *inf = fopen(argv[1], "rb");
+  bool dump_prg = false;
+  if (0 == strcmp(argv[1], "-prg")) {
+    dump_prg = true;
+  } else if (0 == strcmp(argv[1], "-chr")) {
+    dump_prg = false;
+  } else {
+    fprintf(stderr, "First argument must be -chr or -prg.\n");
+    return -1;
+  }
+
+  FILE *inf = fopen(argv[2], "rb");
   if (inf == 0) {
     fprintf(stderr, "Can't read %s\n", argv[1]);
     return -1;
   }
 
-  FILE *outf = fopen(argv[2], "wb");
+  FILE *outf = fopen(argv[3], "wb");
   if (outf == 0) {
     fclose(inf);
-    fprintf(stderr, "Can't open %s for writing\n", argv[2]);
+    fprintf(stderr, "Can't open %s for writing\n", argv[3]);
     return -1;
   }
 
@@ -38,22 +48,38 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  int rombytes = header[4] * 16384;
-  unsigned char *rom = (unsigned char *)malloc(rombytes);
-  if (rombytes != fread(rom, 1, rombytes, inf)) {
-    fprintf(stderr, "Couldn't read %d rom bytes?\n", rombytes);
+  int prg_bytes = header[4] * 16384;
+  int chr_bytes = header[5] * 8192;
+  unsigned char *prg = (unsigned char *)malloc(prg_bytes);
+  if (prg_bytes != fread(prg, 1, prg_bytes, inf)) {
+    fprintf(stderr, "Couldn't read %d PRG bytes?\n", prg_bytes);
     return -1;
   }
 
-  if (rombytes != fwrite(rom, 1, rombytes, outf)) {
-    fprintf(stderr, "Couldn't write %d rom bytes?\n", rombytes);
+  unsigned char *chr = (unsigned char *)malloc(chr_bytes);
+  if (chr_bytes != fread(chr, 1, chr_bytes, inf)) {
+    fprintf(stderr, "Couldn't read %d CHR bytes?\n", chr_bytes);
     return -1;
   }
 
-  fprintf(stderr, "Successfully wrote %d ROM Bytes to %s.\n",
-          rombytes, argv[2]);
+  if (dump_prg) {
+    if (prg_bytes != fwrite(prg, 1, prg_bytes, outf)) {
+      fprintf(stderr, "Couldn't write %d rom bytes?\n", prg_bytes);
+      return -1;
+    }
+    fprintf(stderr, "Successfully wrote %d PRG Bytes to %s.\n",
+            prg_bytes, argv[3]);
+  } else {
+    if (chr_bytes != fwrite(chr, 1, chr_bytes, outf)) {
+      fprintf(stderr, "Couldn't write %d rom bytes?\n", chr_bytes);
+      return -1;
+    }
+    fprintf(stderr, "Successfully wrote %d CHR Bytes to %s.\n",
+            chr_bytes, argv[3]);
+  }
 
-  free(rom);
+  free(chr);
+  free(prg);
   fclose(inf);
   fclose(outf);
   return 0;
