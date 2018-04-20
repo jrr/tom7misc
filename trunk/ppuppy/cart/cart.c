1b6a3a9f971fb00fa6a3e596ca93b74f76fe5de9
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-04-20 13:14:04 +0000

    version with 'animation'

--- trunk/ppuppy/cart/cart.c
+++ trunk/ppuppy/cart/cart.c
@@ -1,53 +1,89 @@
 
-#define PPU_CTRL                *((unsigned char*)0x2000)
-#define PPU_MASK                *((unsigned char*)0x2001)
-#define PPU_STATUS              *((unsigned char*)0x2002)
-#define SCROLL                  *((unsigned char*)0x2005)
-#define PPU_ADDRESS             *((unsigned char*)0x2006)
-#define PPU_DATA                *((unsigned char*)0x2007)
+#define PPU_CTRL                *((unsigned char*)0x2000U)
+#define PPU_MASK                *((unsigned char*)0x2001U)
+#define PPU_STATUS              *((unsigned char*)0x2002U)
+#define SCROLL                  *((unsigned char*)0x2005U)
+#define PPU_ADDRESS             *((unsigned char*)0x2006U)
+#define PPU_DATA                *((unsigned char*)0x2007U)
 
-//      Globals
+// Two writes to this memory-mapped register.
+#define SET_PPU_ADDRESS(addr) \
+  do { \
+    PPU_ADDRESS = (addr >> 8) & 0xff; \
+    PPU_ADDRESS = addr & 0xff; \
+  } while (0)
+
+// Globals
 unsigned char index;
+// Incremented whenever NMI happens.
+unsigned char client_nmi;
 
-const unsigned char TEXT[]={
-"Hello World!"};
+unsigned char prev;
 
-const unsigned char PALETTE[]={
-0x1f, 0x00, 0x10, 0x20
-}; //   black, gray, lt gray, white
+const unsigned char TEXT[] = {
+  "Hello World!"
+};
+
+// Greys from darkest to lightest:
+// 1D 2D 00 10 3D 20
+// In fceux there are really just four
+// distinct colors: black (e.g. 1f),
+// dark grey (e.g. 2d and 00)
+// light grey (e.g. 10 and 3d)
+// and white (e.g. 20, 30)
+
+// So here are some combinations of greys and sepias.
+const unsigned char PALETTE[] = {
+  0x1f, 0x00, 0x10, 0x20,
+  0x1f, 0x08, 0x18, 0x28,
+  0x1f, 0x18, 0x28, 0x20,
+  0x1f, 0x00, 0x10, 0x08,
+};
 
 
 void main() {
-  //    turn off the screen
+  // turn off the screen
   PPU_CTRL = 0;
   PPU_MASK = 0;
 
-  //    load the palette
-  PPU_ADDRESS = 0x3f;   //      set an address in the PPU of 0x3f00
-  PPU_ADDRESS = 0x00;
+  client_nmi = 0;
+  prev = 0;
 
+  // load the palette
+  // set an address in the PPU of 0x3f00
+  SET_PPU_ADDRESS(0x3f00U);
   for (index = 0; index < sizeof(PALETTE); ++index) {
     PPU_DATA = PALETTE[index];
   }
 
-  //load the text
-  PPU_ADDRESS = 0x21;   //      set an address in the PPU of 0x21ca
-  PPU_ADDRESS = 0xca;   //      about the middle of the screen
+  // load the text
+  // This is about the middle of the screen
+  SET_PPU_ADDRESS(0x21caU);
   for (index = 0; index < sizeof(TEXT); ++index) {
     PPU_DATA = TEXT[index];
   }
 
-  //    reset the scroll position
-  PPU_ADDRESS = 0;
-  PPU_ADDRESS = 0;
+  // reset the scroll position
+  SET_PPU_ADDRESS(0x0000U);
   SCROLL = 0;
   SCROLL = 0;
 
-  //    turn on screen
-  PPU_CTRL = 0x90;      //      screen is on, NMI on
+  // turn on screen
+  PPU_CTRL = 0x90;      // screen is on, NMI on
   PPU_MASK = 0x1e;
 
-  //    infinite loop
-  while (1);
+  // Game loop. This can be interrupted at any moment by NMI
+  for (;;) {
+    if (client_nmi) {
+      SET_PPU_ADDRESS(0x21ca);
+      PPU_DATA = prev++;
+
+      SET_PPU_ADDRESS(0x0000U);
+      SCROLL = 0;
+      SCROLL = 0;
+
+      client_nmi = 0;
+    }
+  }
 };
 
