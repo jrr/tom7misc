3e32694a0b79bb72f7651be5eebc323b2d79b770
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-04-28 15:11:13 +0000

    Some slides. Improve makefile/deconvert tool

--- ppuppy/deconvert.cc
+++ ppuppy/deconvert.cc
@@ -7,6 +7,7 @@
 #include "screen.h"
 #include "convert.h"
 #include "base/stringprintf.h"
+#include "util.h"
 
 using namespace std;
 
@@ -123,20 +124,46 @@ ImageRGBA Swatches() {
   return ImageRGBA(std::move(rgba), 256, 256);
 }
 
+static void Save(const ImageRGBA &img,
+		 const string &outfile) {
+  img.Save(outfile);
+  fprintf(stderr, "Wrote %s\n", outfile.c_str());
+}
+
+
 
 int main(int argc, char **argv) {
-  CHECK(argc == 2) << "./deconvert inputgraphic.ext";
-  string infile = argv[1];
+  const char *USAGE = "./deconvert [-multi] input.ext out.png\n";
+  CHECK(argc >= 2) << USAGE;
+  bool multi = false;
 
-  vector<Screen> screens = {ScreenFromFileDithered(infile)};
-  for (int i = 0; i < screens.size(); i++) {
-    const Screen &screen = screens[i];
-    const string outfile = StringPrintf("deconverted-%d.png", i);
+  int argi = 1;
+  if (0 == strcmp(argv[argi], "-multi")) {
+    multi = true;
+    argi++;
+  }
+
+  CHECK(argi < argc) << USAGE;
+  string infile = argv[argi++];
+  CHECK(argi < argc) << USAGE;
+  string base = argv[argi++];
+  
+  CHECK(Util::endswith(base, ".png")) << base;
+  base.resize(base.size() - 4);
+
+  if (multi) {
+    vector<Screen> screens = MultiScreenFromFile(infile);
+    for (int i = 0; i < screens.size(); i++) {
+      const Screen &screen = screens[i];
+      const string outfile = StringPrintf("%s-%d.png", base.c_str(), i);
+      ImageRGBA img = Deconvert(screen);
+      Save(img, outfile);
+    }
+  } else {
+    Screen screen = ScreenFromFile(infile);
     ImageRGBA img = Deconvert(screen);
     // ImageRGBA img = Swatches();
-
-    img.Save(outfile);
-    fprintf(stderr, "Wrote %s\n", outfile.c_str());
+    Save(img, base + ".png");
   }
   return 0;
 }
