6af9401ed83bf0407b8ec8783205e9a17f55f705
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-05-11 14:08:20 +0000

    Allow modifying the delay via the controller. This version was good on 'talk cart'

--- trunk/ppuppy/startup.sh
+++ trunk/ppuppy/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # run as root!
-echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+# echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 tvservice -o
