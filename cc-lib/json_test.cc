e93ef2612ed1651d1c204f5fecb9bb6b175163a9
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-05-24 01:36:14 +0000

    Remove TAO Json because we do not need two JSON parsers and I think i like rapidjson's interface better. Streamline the license there by removing VS 2013 support. Update docs

--- cc-lib/json_test.cc
+++ cc-lib/json_test.cc
@@ -1,24 +1,10 @@
 
 #include "rapidjson/document.h"
 
-#include "taojson/json.hpp"
 #include "base/stringprintf.h"
 #include "base/logging.h"
 
-
-// Test of taocpp json library, and a simple example.
-static void TestTao() {
-  using namespace tao;
-  json::value v = json::from_string( R"({ "a": 5, "b": "hello" })" );
-
-  printf("Pretty:\n%s\n", json::to_string(v).c_str());
-
-  CHECK(v.type() == json::type::OBJECT);
-  CHECK(v.is_object());
-  CHECK(v.get<json::type::OBJECT>()["a"] == 5);
-  CHECK(v.get_object()["b"] == "hello");
-}
-
+// Test of rapidjson library, and a simple example.
 static void TestRapid() {
   using namespace rapidjson;
   Document document;
@@ -43,7 +29,6 @@ static void TestRapid() {
 }
 
 int main(int argc, char **argv) {
-  TestTao();
   TestRapid();
   return 0;
 }
