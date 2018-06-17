58958155d26e19ebf2d516a0a454128022a11ad4
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-06-17 14:51:23 +0000

    ImageRGBA routines

--- trunk/cc-lib/image.cc
+++ trunk/cc-lib/image.cc
@@ -34,6 +34,11 @@ ImageRGBA::ImageRGBA(const vector<uint8> &rgba, int width, int height)
   CHECK(rgba.size() == width * height * 4);
 }
 
+ImageRGBA::ImageRGBA(int width, int height)
+  : width(width), height(height), rgba(width * height * 4) {
+  Clear(0, 0, 0, 0);
+}
+
 void ImageRGBA::Save(const std::string &filename) const {
   CHECK(rgba.size() == width * height * 4);
   stbi_write_png(filename.c_str(), width, height, 4, rgba.data(), 4 * width);
@@ -44,11 +49,53 @@ ImageRGBA *ImageRGBA::Copy() const {
 }
 
 ImageRGBA::uint32 ImageRGBA::GetPixel(int x, int y) const {
+  // Treat out-of-bounds reads as containing 00,00,00,00.
+  if (x < 0 || x >= width ||
+      y < 0 || y >= height) return 0;
   const int base = (y * width + x) << 2;
   return (rgba[base] << 24) |
     (rgba[base + 1] << 16) |
     (rgba[base + 2] << 8) |
     rgba[base + 3];
+}
+
+void ImageRGBA::Clear32(uint32 color) {
+  // PERF: This can be optimized by writing 32 bits at a time,
+  // but beware endianness, etc.
+  Clear((color >> 24) & 255,
+	(color >> 16) & 255,
+	(color >> 8) & 255,
+	color & 255);
+}
+
+void ImageRGBA::Clear(uint8 r, uint8 g, uint8 b, uint8 a) {
+  for (int i = 0; i < width * height * 4; i += 4) {
+    rgba[i + 0] = r;
+    rgba[i + 1] = g;
+    rgba[i + 2] = b;
+    rgba[i + 3] = a;
+  }
+}
+
+void ImageRGBA::SetPixel(int x, int y,
+			 uint8 r, uint8 g, uint8 b, uint8 a) {
+  if (x < 0 || x >= width ||
+      y < 0 || y >= height) return;
+  int i = (y * width + x) * 4;
+  rgba[i + 0] = r;
+  rgba[i + 1] = g;
+  rgba[i + 2] = b;
+  rgba[i + 3] = a;
+}
+
+void ImageRGBA::SetPixel32(int x, int y, uint32 color) {
+  if (x < 0 || x >= width ||
+      y < 0 || y >= height) return;
+  int i = (y * width + x) * 4;
+  rgba[i + 0] = (color >> 24) & 255;
+  rgba[i + 1] = (color >> 16) & 255;
+  rgba[i + 2] = (color >>  8) & 255;
+  rgba[i + 3] = (color      ) & 255;
 }
 
 ImageA::ImageA(const vector<uint8> &alpha, int width, int height)
--- trunk/cc-lib/image.h
+++ trunk/cc-lib/image.h
@@ -1,4 +1,5 @@
 // Images residing in RAM as vectors of pixels.
+// Goal is to be simple and portable.
 
 #ifndef __IMAGE_H
 #define __IMAGE_H
@@ -10,10 +11,13 @@
 #include "base/logging.h"
 
 // 4-channel image in R-G-B-A order.
+// uint32s are represented like 0xRRGGBBAA irrespective of native
+// byte order.
 struct ImageRGBA {
   using uint8 = uint8_t;
   using uint32 = uint32_t;
   ImageRGBA(const std::vector<uint8> &rgba, int width, int height);
+  ImageRGBA(int width, int height);
   
   static ImageRGBA *Load(const std::string &filename);
   void Save(const std::string &filename) const;
@@ -22,6 +26,12 @@ struct ImageRGBA {
 
   // In RGBA order, where R value is MSB. x/y must be in bounds.
   uint32 GetPixel(int x, int y) const;
+  // Clear the image to a single value.
+  void Clear(uint8 r, uint8 g, uint8 b, uint8 a);
+  void Clear32(uint32 rgba);
+
+  void SetPixel(int x, int y, uint8 r, uint8 g, uint8 b, uint8 a);
+  void SetPixel32(int x, int y, uint32 rgba);
   
   const int width, height;
   // Size width * height * 4.
