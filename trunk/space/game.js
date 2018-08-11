43eb9d7e837ab10ceb45976b58516a54b0226b21
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-08-11 23:53:40 +0000

    Some sentence construction; still busted

--- trunk/space/game.js
+++ trunk/space/game.js
@@ -1,14 +1,24 @@
 
-// TODO: Clean this out and replace with a simple game.
-
 let counter = 0, skipped = 0;
 let start_time = (new Date()).getTime();
+
+let mousex = 0;
+let mousey = 0;
 
 // no y-scrolling.
 let scrollx = 400;
 
 // Number of elapsed frames in the current scene.
 let frames = 0;
+
+// If null, no active sentence.
+// Otherwise, we have
+//  { verb: (one of the VERB enums)
+//    obj1: (use OBJ1 with OBJ2)
+//    obj2: (if necessary) }
+// obj can be Ent or Item, or ...
+
+let sentence = null;
 
 const resources = new Resources(
   ['font.png',
@@ -54,6 +64,10 @@ const STARCOLORS = [
 function Init() {
   window.font = new Font(resources.Get('font.png'),
                          FONTW, FONTH, FONTOVERLAP, FONTCHARS);
+  window.hifont = new Font(
+    EzRecolor(resources.Get('font.png'), 0xFFFFFFFF, 0xFF00FFFF),
+    FONTW, FONTH, FONTOVERLAP, FONTCHARS);
+  
   window.spacefont = new Font(resources.Get('spacefont.png'),
                               FONTW, FONTH, FONTOVERLAP, FONTCHARS);
   window.hispacefont = new Font(
@@ -87,7 +101,8 @@ function Init() {
 }
 
 // Item that can go in inventory.
-function Item(invframes, worldframes, mask) {
+function Item(name, invframes, worldframes, mask) {
+  this.name = name;
   // Frames when in inventory
   this.invframes = EzFrames(invframes);
   this.worldframes = EzFrames(worldframes);
@@ -98,10 +113,15 @@ function Item(invframes, worldframes, mask) {
   this.invy = null;
 
   // Position in world (global coordinates, not screen), or null.
+  // (Position is of top-left coordinate)
   // Can't have both world position and inventory position,
   // but an unspawned item could have both be null.
   this.worldx = null;
   this.worldx = null;
+
+  // width/height in world. derived from first frame.
+  this.worldw = this.worldframes.width;
+  this.worldh = this.worldframes.height;
   
   // TODO: state it goes back to if dropped?
   
@@ -117,17 +137,21 @@ Item.prototype.MaskAt = function(x, y) {
 
 function InitGame() {
 
+  scrollx = 5 * WIDTH;
+  
   window.phase = PHASE_GAME;
 
   window.ents = {};
-  window.ents.grateguy = new Ent(['grateguy', 1],
+  window.ents.grateguy = new Ent('ALIEN',
+				 ['grateguy', 1],
 				 // no moving anim
 				 ['grateguy', 1],
 				 30, 65);
   window.ents.grateguy.worldx = 1140;
   window.ents.grateguy.worldy = 91;
 
-  window.player = new Ent(['player1', 1],
+  window.player = new Ent('ME',
+			  ['player1', 1],
 			  ['player1', 9,
 			   'player2', 2,
 			   'player3', 6,
@@ -156,16 +180,23 @@ function InitGame() {
   window.inventoryopen = false;
 
   window.items = {};
-  window.items.egg1 = new Item(['invegg', 1], ['invegg', 1], MASK2x2);
-  window.items.egg2 = new Item(['invegg', 1], ['invegg', 1], MASK2x2);
-  window.items.egg3 = new Item(['invegg', 1], ['invegg', 1], MASK2x2);
-  window.items.egg4 = new Item(['invegg', 1], ['invegg', 1], MASK2x2);
+  window.items.egg1 = new Item('EGG', ['invegg', 1], ['invegg', 1], MASK2x2);
+  window.items.egg2 = new Item('EGG', ['invegg', 1], ['invegg', 1], MASK2x2);
+  window.items.egg3 = new Item('EGG', ['invegg', 1], ['invegg', 1], MASK2x2);
+  window.items.egg4 = new Item('EGG', ['invegg', 1], ['invegg', 1], MASK2x2);
 
-  // XXX?
-  window.items.egg1.invx = 1;
+  // Start with four eggs
+  window.items.egg1.invx = 0;
   window.items.egg1.invy = 0;
+  window.items.egg2.invx = 2;
+  window.items.egg2.invy = 0;
+  window.items.egg3.invx = 4;
+  window.items.egg3.invy = 0;
+  window.items.egg4.invx = 6;
+  window.items.egg4.invy = 0;
   
-  window.items.airlocktool = new Item(['invairlocktool', 1],
+  window.items.airlocktool = new Item('TOOL',
+				      ['invairlocktool', 1],
 				      ['airlocktool', 1],
 				      [' **',
 				       ' * ',
@@ -174,19 +205,22 @@ function InitGame() {
   window.items.airlocktool.worldx = 80;
   window.items.airlocktool.worldy = 48;
   
-  window.items.id = new Item(['invid', 1],
+  window.items.id = new Item('CARD',
+			     ['invid', 1],
 			     ['id', 1],
 			     MASK1x1);
 
-  window.items.id.worldx = 16;
-  window.items.id.worldy = 80;
+  window.items.id.worldx = WIDTH * 5 + 30;
+  window.items.id.worldy = 140;
   
   console.log('initialized game');
 }
 
 // Entity in game that can walk around, speak, etc.
 // Includes the player character.
-function Ent(stand, move, wd, ht) {
+function Ent(name, stand, move, wd, ht) {
+  this.name = name;
+  
   this.halfwidth = wd >>> 1;
   this.height = ht;
 
@@ -310,6 +344,59 @@ function DrawEntsWhen(cond) {
   }
 }
 
+function ExtendSentence(noun) {
+  if (sentence == null) {
+    return { verb: VERB_LOOK, obj1 : noun, obj2 : null };
+  } else if (sentence.obj1 == null) {
+    return { verb: sentence.verb, obj1 : noun, obj2 : null };
+  } else {
+    if (sentence.verb == VERB_OVO ||
+	sentence.verb == VERB_USE && sentence.obj2 == null) {
+      return { verb: sentence.verb, obj1 : sentence.obj1, obj2 : noun };
+    }
+  }
+}
+
+// Draw the sentence (argument) at the top of the screen.
+// Note this is also used when hovering, which happens before
+// the sentence is actually saved.
+function DrawSentence(s) {
+  if (s == null) return;
+  // Nice to show this with colors since it's in space language :)
+  let ll = [];
+  let OneObject = (v) => {
+    if (s.obj1 != null)
+      return [v, s.obj1.name];
+    else
+      return [v];
+  };
+
+  let TwoObjects = (v, c) => {
+    if (s.obj2 != null)
+      return [v, s.obj1.name, c, s.obj2.name];
+    else if (s.obj1 != null)
+      return [v, s.obj1.name, c];
+    else
+      return [v];
+  };
+
+  switch(s.verb) {
+  case VERB_LOOK: ll = OneObject("LOOK AT"); break;
+  case VERB_GRAB: ll = OneObject("GRAB"); break;
+  case VERB_TALK: ll = OneObject("TALK TO"); break;
+  case VERB_OVO: ll = TwoObjects("OVOPOSIT", "INTO"); break;
+  case VERB_USE: ll = TwoObjects("USE", "WITH"); break;
+  case VERB_DROP: ll = OneObject("DROP"); break;
+  }
+
+  let x = 1;
+  for (let i = 0; i < ll.length; i++) {
+    let ph = ll[i];
+    ((i % 0 == 1) ? hifont : font).Draw(ctx, x, 1, ph);
+    x += (ph.length + 1) * (FONTW - FONTOVERLAP);
+  }
+}
+
 function DrawGame() {
   ClearScreen();
   DrawStars();
@@ -320,14 +407,16 @@ function DrawGame() {
   // TODO: hover/open state?
   DrawFrame(window.inv_icon, INVICON.x, INVICON.y);
 
-  // TODO: hover/active states
-  spacefont.Draw(ctx, 64, 180, "GRAB");
-  hispacefont.Draw(ctx, 110, 180, "TALK");
-  hispacefont.Draw(ctx, 158, 180, "OVOPOSIT");
-  spacefont.Draw(ctx, 238, 180, "USE");
-  spacefont.Draw(ctx, 277, 180, "DROP");
-
-  font.Draw(ctx, 1, 1, "USE ID WITH CARD READER");
+  for (let act of ACTIONS) {
+    // XXX conditions for highlighting? are they always clickable?
+    let canhighlight = sentence == null;
+    let f = (canhighlight && InRect(mousex, mousey, act)) ?
+	hispacefont : spacefont;
+    f.Draw(ctx, act.x, act.y, act.text);
+  }
+    
+  // font.Draw(ctx, 1, 1, "USE ID WITH CARD READER");
+  DrawSentence(sentence);
 
   // Objects need to be clipped by the top deck, so only
   // draw lower deck items first...
@@ -390,7 +479,7 @@ function Draw() {
   }
 }
 
-last = 0;
+let last = 0;
 function Step(time) {
   // Throttle to 30 fps or something we
   // should be able to hit on most platforms.
@@ -506,15 +595,28 @@ function InRect(x, y, r) {
       y >= r.y && y < r.y + r.h;
 }
 
-/*
-function InRect(x, y, x0, y0, w, h) {
-  return x >= x0 && x < x + w &&
-      y >= y0 && y < y + h;
+function InCoords(x, y, x0, y0, w, h) {
+  return x >= x0 && x < x0 + w &&
+      y >= y0 && y < y0 + h;
 }
-*/
 
 function CanvasMousedownGame(x, y) {
-  console.log('click ', x, y, " = global ", scrollx + x, y);
+  let globalx = scrollx + x;
+  console.log('click ', x, y, " = global ", globalx, y);
+
+  // If we have no sentence, we can always pick a verb
+  // (XXX: NOT if we've grabbed an item and need to put it in
+  // inventory, though)
+  if (sentence == null && y > ACTY) {
+    for (let act of ACTIONS) {
+      if (InRect(mousex, mousey, act)) {
+	console.log('start sentence ', VerbString(act.verb));
+	sentence = { verb: act.verb, obj1: null, obj2: null };
+	return;
+      }
+    }
+  }
+
   if (window.inventoryopen) {
     // TODO: First check action clicks, since these are the
     // only thing outside the inventory itself that don't close it
@@ -538,9 +640,21 @@ function CanvasMousedownGame(x, y) {
       return;
     }
 
+    // In an item?
+    for (let o in items) {
+      let item = items[o];
+      if (item.worldx != null &&
+	  InCoords(globalx, y,
+		   item.worldx, item.worldy,
+		   item.worldw, item.worldh)) {
+	// see if we can add to sentence
+	sentence = ExtendSentence(item);
+      }
+    }
+	 
     // XXX test that it's in bounds, not item, etc.
     // (double-click to walk?!)
-    player.targetx = scrollx + x;
+    player.targetx = globalx;
     player.targety = y;
     
     // TODO:
@@ -595,8 +709,8 @@ function CanvasMove(event) {
   var bcy = bigcanvas.canvas.offsetTop;
   var x = Math.floor((event.pageX - bcx) / PX);
   var y = Math.floor((event.pageY - bcy) / PX);
-  window.mousex = x;
-  window.mousey = y;
+  mousex = x;
+  mousey = y;
 
   // If we use movement anywhere else (unlikely!) then
   // do the switch thing.
