a1dbedba992f34f1d180347e567f23d1c4592933
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-08-11 23:53:40 +0000

    Some sentence construction; still busted

--- space/constants.js
+++ space/constants.js
@@ -4,7 +4,7 @@ var DEBUG = false; // true;
 var WIDTH = 320;
 var HEIGHT = 200;
 var PX = 3;
-var MINFRAMEMS = 16.0;
+var MINFRAMEMS = 9.0;
 
 var PHASE_TITLE = 1;
 var PHASE_CUTSCENE = 2;
@@ -47,3 +47,40 @@ const MAXSTARS = 50;
 const GAMEWIDTH = 6 * WIDTH;
 
 const SCROLLMARGIN = 120;
+
+const VERB_LOOK = 1;
+const VERB_GRAB = 2;
+const VERB_TALK = 3;
+const VERB_OVO = 4;
+const VERB_USE = 5;
+const VERB_DROP = 6;
+function VerbString(v) {
+  switch(v) {
+  case VERB_LOOK: return "LOOK";
+  case VERB_GRAB: return "GRAB";
+  case VERB_TALK: return "TALK";
+  case VERB_OVO: return "OVOPOSIT";
+  case VERB_USE: return "USE";
+  case VERB_DROP: return "DROP";
+  }
+}
+
+const ACTY = 180;
+
+function Action(x, text, verb) {
+  this.verb = verb;
+  this.x = x;
+  this.y = ACTY;
+  this.text = text;
+  this.w = (FONTW - FONTOVERLAP) * text.length;
+  this.h = FONTH;
+  return this;
+}
+const GRABACT = new Action(64, "GRAB", VERB_GRAB);
+const TALKACT = new Action(110, "TALK", VERB_TALK);
+const OVOACT = new Action(158, "OVOPOSIT", VERB_OVO);
+const USEACT = new Action(238, "USE", VERB_USE);
+// ?
+const DROPACT = new Action(277, "DROP", VERB_DROP);
+const ACTIONS = [GRABACT, TALKACT, OVOACT, USEACT];
+
