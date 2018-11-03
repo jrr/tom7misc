e56a604aab73e10b0ceecaa38c4bd98f4b429568
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-11-03 20:59:44 +0000

    Tested on hundreds of millions of games so I think it's likely correct now, at least for the sake of accepting legal moves. Add skeleton of paper with my guesses prior to getting any results. Some performance improvements. Finally, track fates of pieces as necessary for the computation.

--- trunk/chess/pgn.h
+++ trunk/chess/pgn.h
@@ -6,9 +6,9 @@
 #include <vector>
 #include <string>
 
+#include "re2.h"
+
 struct PGN {
-  // Parses a subset of the PGN language. Returns false upon failure.
-  static bool Parse(const std::string &s, PGN *pgn);
 
   enum Result {
     WHITE_WINS,
@@ -16,6 +16,10 @@ struct PGN {
     DRAW,
     OTHER,
   };
+
+  // If you are parsing a large number of PGNs, it is slightly
+  // faster to make a PGNParser instance and reuse it.
+  static bool Parse(const std::string &s, PGN *pgn);
   
   std::unordered_map<std::string, std::string> meta;
   struct Move {
@@ -28,6 +32,15 @@ struct PGN {
   // Does not include the terminating event like 1-0.
   std::vector<Move> moves;
   Result result;
+};
+
+struct PGNParser {
+  // Parses a subset of the PGN language. Returns false upon failure.
+  bool Parse(const std::string &s, PGN *pgn) const;
+  PGNParser();
+  
+private:
+  const RE2 meta_line_re, move_re, end_re;
 };
 
 #endif
