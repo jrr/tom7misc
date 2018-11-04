f81f23e39dd7fde15ec99bb38920261ea3b372ec
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-11-04 15:03:24 +0000

    Batch rungames working. Produce basic report.

--- chess/rungames.cc
+++ chess/rungames.cc
@@ -10,9 +10,11 @@
 #include <unistd.h>
 
 #include "base/logging.h"
-#include "pgn.h"
 #include "util.h"
 #include "city.h"
+
+#include "pgn.h"
+#include "gamestats.h"
 
 using namespace std;
 using int64 = int64_t;
@@ -180,51 +182,6 @@ private:
   // This is set to true when no more work will be added,
   // which signals that the workers can exit.
   bool no_more_work = false;
-};
-
-// Whole purpose of running over games is to collect these stats. So
-// that we can estimate variance, we also randomly assign players to
-// one of G groups (by hash of username) and accumulate a given game
-// into the white player's bucket. So there are actually G Stats
-// objects.
-static constexpr int NUM_BUCKETS = 32;
-static_assert(! (NUM_BUCKETS & (NUM_BUCKETS - 1)),
-	      "Must be a power of two");
-static constexpr int64 NUM_BUCKETS_MASK = NUM_BUCKETS - 1;
-
-// Results of a single game.
-struct GameStats {
-  static constexpr uint8 DIED = 0b10000000;
-  static constexpr uint8 POS_MASK = 0b00111111;
-  // Initialized to their start squares (alive) in standard position.
-  uint8 fates[32] = {
-		      0,  1,  2,  3,  4,  5,  6,  7,
-		      8,  9, 10, 11, 12, 13, 14, 15,
-		     48, 49, 50, 51, 52, 53, 54, 55,
-		     56, 57, 58, 59, 60, 61, 62, 63, };
-};
-
-struct PieceStats {
-  int64 died_on[64] = {};
-  int64 survived_on[64] = {};
-};
-
-struct Stats {
-  std::shared_mutex m;
-  int64 num_games = 0LL;
-  PieceStats pieces[32] = {};
-  void AddGame(const GameStats &gs) {
-    WriteMutexLock ml(&m);
-    num_games++;
-    for (int i = 0; i < 32; i++) {
-      const uint8 fate = gs.fates[i];
-      if (fate & GameStats::DIED) {
-	pieces[i].died_on[fate & GameStats::POS_MASK]++;
-      } else {
-	pieces[i].survived_on[fate & GameStats::POS_MASK]++;
-      }
-    }
-  }
 };
 
 
