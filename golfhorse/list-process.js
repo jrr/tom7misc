66ed7052d799807877afa789618c8b91f31b08c2
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-11-07 04:09:32 +0000

    More compact version of this

--- golfhorse/list-process.js
+++ golfhorse/list-process.js
@@ -1,2 +1,1 @@
-for(i=0;i<r.length;i+=2)s=s.split(r[i]).join(r[i+1])
-w='';for(i=0;i<s.length;){c=s[i++];d=c.charCodeAt(0)-48;if(d>=0&&d<=9){console.log(w);w=w.substr(0,d);}else w+=c}
+w='';for(i=0;i<s.length;){c=s[i++];if(+c+1){console.log(w);w=w.substr(0,+c);}else w+=c}
