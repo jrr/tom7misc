3f32f822d6d986946aaf36538ecd87304b31cbc8
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-11-08 04:02:00 +0000

    Tidy up the decoder code, saving a few bytes. Ideas in makedata.

--- trunk/golfhorse/makeutf.cc
+++ trunk/golfhorse/makeutf.cc
@@ -11,18 +11,22 @@ int main (int argc, char **argv) {
 
     b1 |= (i >> 6) & 0b11111;
     b2 |= i & 0b111111;
-    printf("'%c%c',", b1, b2);
+    printf("'e%c%co',", b1, b2);
   }
   printf("];\n");
 
   printf(R"(
 for (let i = 0; i < r.length; i++) {
-  if (r[i].length != 1 || r[i][0] != r[i]) {
+  if (r[i].length != 3 || r[i][1] != r[i].substr(1, 1)) {
     console.log(i, ' no: ', r[i], " len ", r[i].length, 
                 " ", r[i].charCodeAt(0), " ", r[i].charCodeAt(1));
   }
+  if (r[i].split(r[i][1]).join('z') != 'ezo')
+    console.log(i, 'split/join');
 }
 )");
+
+  
   
   return 0;
 }
