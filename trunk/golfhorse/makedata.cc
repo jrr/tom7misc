ed60a413d0d698181e4dff72e9b7a071a79013f4
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2018-11-10 14:33:52 +0000

    Allow low ascii. Use binary output mode to avoid carriage returns, and embed decoder to avoid cat/dos2unix. Move file output to its own routine. Verbosity levels. Hoist some constants.

--- trunk/golfhorse/makedata.cc
+++ trunk/golfhorse/makedata.cc
@@ -7,8 +7,24 @@
 #include "util.h"
 #include "../cc-lib/threadutil.h"
 #include "base/stringprintf.h"
+#include "base/logging.h"
 
 using namespace std;
+
+// This is currently assumed.
+static constexpr bool all_onecodepoint_source = true;
+
+#undef WRITE_LOG
+
+// static constexpr int OUTER_THREADS = 1;
+static constexpr int INNER_THREADS = 56;
+
+// 0 - Silent
+// 1 - Print e.g. termination reason
+// 3 - Print the replacements we actually make
+// 4 - Print the replacements we try
+// 5 - Print the strings found by each inner thread
+static constexpr int VERBOSE = 3;
 
 static int SharedPrefix(const string &a, const string &b) {
   int i = 0;
@@ -20,6 +36,7 @@ static int SharedPrefix(const string &a, const string &b) {
 // overlap.
 static int Occurrences(const string &data,
 		       const char *subs, size_t sublen) {
+  if (sublen == 0) return 0;
   size_t start = 0;
   int occ = 0;
   for (;;) {
@@ -32,9 +49,37 @@ static int Occurrences(const string &data,
 }
 
 static int Occurrences(const string &data, const string &subs) {
-  if (subs.size() == 0) return 0;
   return Occurrences(data, subs.c_str(), subs.size());
 }
+
+inline static int TabledOccurrences(const vector<vector<int>> &table,
+				    const string &data,
+				    const char *subs,
+				    size_t sublen) {
+  if (sublen == 0) return 0;
+  const uint8 c = *subs;
+  const vector<int> &starts = table[c];
+
+  int occ = 0;
+  int next_valid_start = 0;
+  for (int start : starts) {
+    if (start + sublen >= data.size())
+      break;
+    
+    if (start >= next_valid_start) {
+      for (int i = 0; i < sublen; i++) {
+	if (data[start + i] != subs[i]) goto next;
+      }
+      // Found a match.
+      occ++;
+      // Don't allow overlapping matches.
+      next_valid_start = start + sublen;
+    }
+  next:;
+  }
+  return occ;
+}
+
 
 // This assumes that we have at most two-byte utf-8 encodings
 // and that the string s is all valid.
@@ -80,8 +125,9 @@ static inline bool TerminatedUnicode(const char *s, int len) {
 
 // Returns the best substring (best = maximum value of (size *
 // occurrences)) and the score.
-static constexpr int LONGEST_EXTENSION = 6;
+static constexpr int LONGEST_EXTENSION = 11;
 static pair<string, int> BestReplacement(
+    const vector<vector<int>> &table,
     int source_length,
     int offset, int stride, const string &s) {
   int best_value = 0;
@@ -93,7 +139,8 @@ static pair<string, int> BestReplacement(
 	 len <= (min_length + LONGEST_EXTENSION) && i + len < s.size();
 	 len++) {
       if (TerminatedUnicode(s.c_str() + i, len)) {
-	const int occ = Occurrences(s, s.c_str() + i, (size_t)len);
+	const int occ =
+	  TabledOccurrences(table, s, s.c_str() + i, (size_t)len);
 	if (occ > 1) {
 	  int cost = 1 + source_length + len;
 	  int savings = occ * (len - source_length);
@@ -113,10 +160,20 @@ static pair<string, int> BestReplacement(
   return make_pair(s.substr(best_idx, best_len), best_value);
 }
 
+// v must be length 256. v[c] contains a vector of all the positions
+// in s, in ascending sorted order, where the character c is.
+void MakeTable(const string &s, vector<vector<int>> *v) {
+  for (vector<int> &vv : *v) vv.clear();
+  for (int i = 0; i < s.size(); i++) {
+    uint8 c = s[i];
+    (*v)[c].push_back(i);
+  }
+}
+
 static string Encode(const string &s) {
   const bool all_ascii =
     [&s](){
-      for (char c : s)
+      for (uint8 c : s)
 	if (c < ' ' || c >= 127)
 	  return false;
       return true;
@@ -126,14 +183,7 @@ static string Encode(const string &s) {
   string ret = "";
   bool unterminated_utf = false;
   for (uint8 c : s) {
-    if (c < ' ') {
-      if (unterminated_utf) {
-	ret += StringPrintf("[BAD UNTERMINATED LOW %02x", (int)c);
-	unterminated_utf = false;
-      } else {
-	ret += StringPrintf("[BAD LOW %02x]", (int)c);
-      }
-    } else if ((c & 0b111'00000) == 0b110'00000) {
+    if ((c & 0b111'00000) == 0b110'00000) {
       // First byte.
       unterminated_utf = true;
       ret += StringPrintf("[%02x]", (int)c);
@@ -149,6 +199,10 @@ static string Encode(const string &s) {
       if (unterminated_utf) {
 	ret += StringPrintf("[BAD UNTERMINATED ASCII %02x=%c]", (int)c, c);
 	unterminated_utf = false;
+      } else if (c < ' ') {
+	ret += StringPrintf("[%02x]", c);
+      } else if (c > 127) {
+	ret += StringPrintf("[BAD HI? %02x]", c);
       } else {
 	ret += c;
       }
@@ -156,6 +210,104 @@ static string Encode(const string &s) {
   }
   if (unterminated_utf) ret += "[BAD UNTERMINATED]";
   return ret;
+}
+
+static void WriteFile(const char *filename,
+		      const string &data,
+		      const vector<pair<string, string>> &reps) {
+  // Note "binary" is important on windows to avoid converting
+  // \n to \r\n.
+  FILE *out = fopen(filename, "wb");
+  CHECK(out) << "Couldn't open " << filename;
+
+  fprintf(out, "s='");
+  CHECK(1 == fwrite(data.data(), data.size(), 1, out));
+  // fprintf(out, "%s", data.c_str());
+  fprintf(out, "'\n");
+  // Need to do the replacements in backwards order.
+
+  auto InReps =
+    [&reps](char c) {
+      for (const auto &p : reps) {
+	if (p.first.find(c) != string::npos ||
+	    p.second.find(c) != string::npos)
+	  return true;
+      }
+      return false;
+    };
+
+  char sep = 0;
+  for (char c :
+	 // Prefer a numeric separator, since we don't
+	 // need to quote these.
+	 "0123456789"
+	 "qwertyuiopasdfghjklzxcvbnm"
+	 "QWERTYUIOPASDFGHJKLZXCVBNM"
+	 "!@#$%^&*()_+[]{}|:;<>?,./ ") {
+    if (!InReps(c)) { sep = c; break; }
+  }
+
+  // TODO: Even better encoding here would be like a number (digit? but
+  // then the maximum length would be 9, or I guess we could assume
+  // the replacement is at least 2, so 11) that gives the length of the
+  // replacement, then the single source codepoint (this would preclude
+  // using two-codepoint ascii sources, which does give us 9025 useful
+  // sources...), then the n characters. so the separator would contain
+  // information. parsing would be a little trickier, but not that bad.
+  // So like 7*destina3zfoo would mean replace * with destina and
+  // z with foo. Actually that is no cheaper than just using a single
+  // character as the separator anyway. Better is like
+  // *7destinaz3foo, with the advantage here being that we don't have
+  // to assume a single codepoint for the source strings, as long as
+  // they can't contain a digit (easy to arrange; we use the digits
+  // for a special purpose anyway). And of course we have a maximum
+  // replacement size if we do that.
+
+  // Note the use of fwrite below instead of fprintf with %s, since
+  // the strings may include 0.
+  if (all_onecodepoint_source && sep != 0) {
+    fprintf(out, "for(o of'");
+    for (int i = reps.size() - 1; i >= 0; i--) {
+      if (i != reps.size() - 1) fprintf(out,"%c",sep);
+      CHECK(1 == fwrite(reps[i].first.data(), reps[i].first.size(), 1, out));
+      CHECK(1 == fwrite(reps[i].second.data(), reps[i].second.size(), 1, out));
+    }
+    string sep_js = (sep >= '0' && sep <= '9') ?
+      StringPrintf("%c", sep) :
+      StringPrintf("'%c'", sep);
+    fprintf(out, "'.split(%s))s=s.split(o[0]).join(o.slice(1))\n",
+	    sep_js.c_str());
+  } else if (all_onecodepoint_source) {
+    fprintf(out, "for(o of[");
+    for (int i = reps.size() - 1; i >= 0; i--) {
+      if (i != reps.size() - 1) fprintf(out,",");
+
+      fprintf(out, "'");
+      CHECK(1 == fwrite(reps[i].first.data(), reps[i].first.size(), 1, out));
+      CHECK(1 == fwrite(reps[i].second.data(), reps[i].second.size(), 1, out));
+      fprintf(out, "'");
+    }
+    fprintf(out, "])s=s.split(o[0]).join(o.slice(1))\n");
+  } else {
+    fprintf(out, "r=[");
+    for (int i = reps.size() - 1; i >= 0; i--) {
+      if (i != reps.size() - 1) fprintf(out,",");
+      fprintf(out, "'");
+      CHECK(1 == fwrite(reps[i].first.data(), reps[i].first.size(), 1, out));
+      fprintf(out, "','");
+      CHECK(1 == fwrite(reps[i].second.data(), reps[i].second.size(), 1, out));
+      fprintf(out, "'");
+    }
+    fprintf(out, "]\n");
+    fprintf(out,
+	    "for(i=0;i<%d;i+=2)s=s.split(r[i]).join(r[i+1])\n",
+	    (int)reps.size());
+  }
+
+  fprintf(out,
+	  "w='';for(c of s)+c+1?w=console.log(w)||w.substr(0,+c):w+=c");
+  
+  fclose(out);
 }
 
 
@@ -211,6 +363,14 @@ int main (int argc, char **argv) {
   // characters like \r \n \t. They are at least as good as utf-16,
   // though maybe tricky to handle in some ways (and we have to be
   // careful about splitting them, like with multibyte utf)
+
+  // According to ES6, anything can appear literally except:
+  // closing quote code points, U+005C (REVERSE SOLIDUS),
+  // U+000D (CARRIAGE RETURN), U+2028 (LINE SEPARATOR),
+  // U+2029 (PARAGRAPH SEPARATOR), and U+000A (LINE FEED). 
+  // (Does this really include U+0000?! crazy)
+  // So good news is we can probably add some low-ascii here, but bad news
+  // is that we need to filter out a couple high unicode codepoints too.
   string sourcechars =
     " \"ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-=+[]{}|;:<>?,./~`";
   for (char c : sourcechars) {
@@ -219,52 +379,65 @@ int main (int argc, char **argv) {
     sources.push_back(ch);
   }
 
-  
-  // Forced to true 
-  constexpr bool all_onecodepoint_source = true;
-  /*
-  for (const auto &p : reps)
-    if (p.first.size() != 1)
-      all_onecodepoint_source = false;
-  */
-  
+  // And low ASCII.
+  for (uint8 c = 0; c < 32; c++) {
+    if (c != 0x0A && c != 0x0D) {
+      string ch = " ";
+      ch[0] = c;
+      sources.push_back(ch);
+    }
+  }
 
   static constexpr int BEST_PER_ROUND = 1;
   // TODO: Periodically write file?
-  static constexpr int MAX_SECONDS = 2 * 3600;
+  static constexpr int MAX_SECONDS = 6 * 3600;
+
+  vector<vector<int>> table;
+  table.resize(256);
+
+  #ifdef WRITE_LOG
+  FILE *log = fopen("log.txt", "w");
+  #endif
   
   int64 start_time = time(nullptr);
   for (;;) {
     int64 time_now = time(nullptr);
     if (time_now - start_time > MAX_SECONDS) {
-      fprintf(stderr, "Ran out of time.\n");
+      if (VERBOSE >= 1)
+	fprintf(stderr, "Ran out of time.\n");
       break;
     }
     
     std::mutex m;
     vector<pair<string, int>> results;
 
-    static constexpr int NUM_THREADS = 56;
     if (sources.empty()) {
-      fprintf(stderr, "No more sources (up)...\n");
+      if (VERBOSE >= 1)
+	fprintf(stderr, "No more sources (up)...\n");
       goto nomore;
     }
     int source_length = sources.back().size();
 
-    ParallelComp(NUM_THREADS,
-		 [&m, &results, &data, source_length](int idx) {
+    MakeTable(data, &table);
+    
+    ParallelComp(INNER_THREADS,
+		 [&m, &results, &data, &table, source_length](int idx) {
 
 		   auto res =
-		     BestReplacement(source_length, idx, NUM_THREADS, data);
+		     BestReplacement(table, source_length, idx,
+				     INNER_THREADS, data);
 		   
 		   {
 		     MutexLock ml(&m);
 		     results.push_back(std::move(res));
 		   }
 		 },
-		 NUM_THREADS);
-    fprintf(stderr, ".\n");
-    fflush(stderr);
+		 INNER_THREADS);
+
+    if (VERBOSE >= 5) {
+      fprintf(stderr, ".\n");
+      fflush(stderr);
+    }
     
     // Sort descending.
     // PERF: Should sort by savings here. Common to see cases where
@@ -275,10 +448,12 @@ int main (int argc, char **argv) {
 		return a.second > b.second;
 	      });
 
-    for (const auto &p : results) {
-      fprintf(stderr, " [%s]x%d\n", p.first.c_str(), p.second);
+    if (VERBOSE >= 5) {
+      for (const auto &p : results) {
+	fprintf(stderr, " [%s]x%d\n", p.first.c_str(), p.second);
+      }
+      fflush(stderr);
     }
-    fflush(stderr);
     
     // Take the overall best (or all of them?)
     bool made_progress = false;
@@ -297,7 +472,8 @@ int main (int argc, char **argv) {
       tried++;
       
       if (sources.empty()) {
-	fprintf(stderr, "No more sources...\n");
+	if (VERBOSE >= 1)
+	  fprintf(stderr, "No more sources...\n");
 	goto nomore;
       }
 	
@@ -312,106 +488,48 @@ int main (int argc, char **argv) {
       static_assert(all_onecodepoint_source, "precondition");
       int cost = 1 + sources.back().size() + dst.size();
       int savings = occ * (dst.size() - sources.back().size());
-      fprintf(stderr, "%s<-%s has %d real occurrences cost %d savings %d.\n",
-	      Encode(dst).c_str(),
-	      Encode(sources.back()).c_str(), occ, cost, savings);
+      if (VERBOSE >= 4 || (VERBOSE >= 3 && savings > cost)) {
+	fprintf(stderr, "%s<-%s has %d real occurrences cost %d savings %d.\n",
+		Encode(dst).c_str(),
+		Encode(sources.back()).c_str(), occ, cost, savings);
+      }
       if (savings > cost) {
 	string src = sources.back();
 	sources.pop_back();
 	reps.push_back(std::make_pair(src, dst));
 	data = Util::Replace(data, dst, src);
-	fprintf(stderr, "Size now %d. %d source(s) left\n",
-		(int)data.size(),
-		(int)sources.size());
+	if (VERBOSE >= 3) {
+	  int source_size = sources.empty() ? 0 : (int)sources.back().size();
+	  fprintf(stderr, "Size now %d. %d source(s) left [size %d]\n",
+		  (int)data.size(),
+		  (int)sources.size(),
+		  source_size);
+	}
 	made_progress = true;
+
+	#ifdef WRITE_LOG
+	fprintf(log, "'%s' <- '%s' has %d real occurrences,"
+		"cost %d savings %d (now %d)\n",
+		Encode(dst).c_str(),
+		Encode(src).c_str(), occ, cost, savings,
+		(int)data.size());
+	fflush(log);
+	#endif
       }
       fflush(stderr);
     }
     if (!made_progress) {
-      fprintf(stderr, "Didn't make progress...");
+      if (VERBOSE >= 1)
+	fprintf(stderr, "Didn't make progress...");
       break;
     }
   }
   nomore:;
   fflush(stderr);
-  
-  FILE *out = fopen(argv[2], "w");
-  if (!out) {
-    fprintf(stderr, "Couldn't open %s\n", argv[2]);
-    return -1;
-  }
-  fprintf(out, "s='");
-  fprintf(out, "%s", data.c_str());
-  fprintf(out, "'\n");
-  // Need to do the replacements in backwards order.
+  #ifdef WRITE_LOG
+  fclose(log);
+  #endif
 
-  auto InReps =
-    [&reps](char c) {
-      for (const auto &p : reps) {
-	if (p.first.find(c) != string::npos ||
-	    p.second.find(c) != string::npos)
-	  return true;
-      }
-      return false;
-    };
-
-  char sep = 0;
-  for (char c :
-	 // Prefer a numeric separator, since we don't
-	 // need to quote these.
-	 "0123456789"
-	 "qwertyuiopasdfghjklzxcvbnm"
-	 "QWERTYUIOPASDFGHJKLZXCVBNM"
-	 "!@#$%^&*()_+[]{}|:;<>?,./ ") {
-    if (!InReps(c)) { sep = c; break; }
-  }
-
-  // TODO: Even better encoding here would be like a number (digit? but
-  // then the maximum length would be 9, or I guess we could assume
-  // the replacement is at least 2, so 11) that gives the length of the
-  // replacement, then the single source codepoint (this would preclude
-  // using two-codepoint ascii sources, which does give us 9025 useful
-  // sources...), then the n characters. so the separator would contain
-  // information. parsing would be a little trickier, but not that bad.
-  // So like 7*destina3zfoo would mean replace * with destina and
-  // z with foo. Actually that is no cheaper than just using a single
-  // character as the separator anyway. Better is like
-  // *7destinaz3foo, with the advantage here being that we don't have
-  // to assume a single codepoint for the source strings, as long as
-  // they can't contain a digit (easy to arrange; we use the digits
-  // for a special purpose anyway). And of course we have a maximum
-  // replacement size if we do that.
-  
-  if (all_onecodepoint_source && sep != 0) {
-    fprintf(out, "for(o of'");
-    for (int i = reps.size() - 1; i >= 0; i--) {
-      if (i != reps.size() - 1) fprintf(out,"%c",sep);
-      fprintf(out, "%s%s",
-	      reps[i].first.c_str(), reps[i].second.c_str());
-    }
-    string sep_js = (sep >= '0' && sep <= '9') ?
-      StringPrintf("%c", sep) :
-      StringPrintf("'%c'", sep);
-    fprintf(out, "'.split(%s))s=s.split(o[0]).join(o.slice(1))\n",
-	    sep_js.c_str());
-  } else if (all_onecodepoint_source) {
-    fprintf(out, "for(o of[");
-    for (int i = reps.size() - 1; i >= 0; i--) {
-      if (i != reps.size() - 1) fprintf(out,",");
-      fprintf(out, "'%s%s'", reps[i].first.c_str(), reps[i].second.c_str());
-    }
-    fprintf(out, "])s=s.split(o[0]).join(o.slice(1))\n");
-  } else {
-    fprintf(out, "r=[");
-    for (int i = reps.size() - 1; i >= 0; i--) {
-      if (i != reps.size() - 1) fprintf(out,",");
-      fprintf(out, "'%s','%s'", reps[i].first.c_str(), reps[i].second.c_str());
-    }
-    fprintf(out, "]\n");
-    fprintf(out,
-	    "for(i=0;i<%d;i+=2)s=s.split(r[i]).join(r[i+1])\n",
-	    (int)reps.size());
-  }
-  fclose(out);
+  WriteFile(argv[2], data, reps);
   return 0;
 }
