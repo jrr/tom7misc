c9a4238054019fc6287aa2f22d5e23f927395e4f
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2019-01-06 05:32:12 +0000

    Add unblinder eval program, but there seems to be something wrong

--- trunk/chess/blind/unblinder-mk0.h
+++ trunk/chess/blind/unblinder-mk0.h
@@ -2,8 +2,14 @@
 #ifndef __UNBLINDER_MK0_H
 #define __UNBLINDER_MK0_H
 
+#include <string>
+
+#include "unblinder.h"
+
 struct UnblinderMk0 {
-  static Unblinder *LoadFromFile(const string &filename);
+  static Unblinder *LoadFromFile(const std::string &filename);
+
+  UnblinderMk0() = delete;
 };
 
 #endif
