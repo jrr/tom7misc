840520110e6d03bf22bb4a0ab59d15e9f600a287
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2019-01-30 04:56:13 +0000

    Start on tournament wrapper

---
+++ chess/player.h
@@ -0,0 +1,31 @@
+
+#ifndef __PLAYER_H
+#define __PLAYER_H
+
+#include "chess.h"
+
+// Interface for a chess-playing algorithm.
+struct Player {
+
+  // Make a move in an arbitrary position. The position will
+  // have at least one legal move (i.e., it is not currently
+  // mate). The move returned must be legal!
+  // This position need not correspond to any previous positions
+  // invoked on, but it is okay for it is okay for it to keep
+  // some state (like a random number generator).
+  virtual Position::Move MakeMove(const Position &pos) = 0;
+
+  // Return the name of the algorithm. Should be distinct
+  // across all implementations of the interface.
+  virtual const char *Name() const = 0;
+  // Brief description of how the player works.
+  virtual const char *Desc() const = 0;
+};
+
+// And some factory functions for players.
+// Each returns a new-ly created object, owned by the caller.
+Player *CreateFirstMove();
+Player *CreateRandom();
+Player *CreateCCCP();
+
+#endif
