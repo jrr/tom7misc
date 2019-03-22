5dff2521da3817a4f84650d1bfa3ae719308816f
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2019-03-22 23:52:37 +0000

    Enable FPU, and confirm that it's working with a simple program

--- trunk/nand/board/rom/main.c
+++ trunk/nand/board/rom/main.c
@@ -1,5 +1,17 @@
 #include "stm32f30x.h"
 
+#include <math.h>
+
+#if !defined(__FPU_PRESENT) || !defined(__FPU_USED)
+# error please define these
+#endif
+
+// FPU is enabled (see section 4.6.6 in the ARM M4F manual)
+// in system_stm32f30x.c if these are turned on.
+#if __FPU_PRESENT != 1 || __FPU_USED != 1
+# error this program depends on floating-point instructions
+#endif
+
 /* zyp's cycle count delay routines */
 volatile uint32_t* demcr = (uint32_t*)0xE000EDFC;
 volatile uint32_t* dwt_ctrl = (uint32_t*)0xe0001000;
@@ -35,7 +47,13 @@ static void InitPinOutB(uint16_t pins) {
   GPIO_Init(GPIOB, &gpio);
 }
 
+#define OUTPUT_MASK ((1<<13) | (1<<14) | (1<<15))
+
 int main(void) {
+  float vinf = 1.0f / 0.0f;
+  float vnan = 0.0f / 0.0f;
+  float w = fmax(1234.0f, vnan);
+
   // Here clock literally means the oscillator that drives
   // this part of the chip, in other words, "turn on the GPIO B
   // peripheral so that we can use it."
@@ -44,28 +62,36 @@ int main(void) {
   /* Enable PB13,14,15 for output */
   InitPinOutB(GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
 
-  GPIOB->BSRR = (1<<13) | (1<<14) | (1<<15);
+  GPIOB->BSRR = OUTPUT_MASK;
 
   /* start cycle counter */
   cyccnt_enable();
 
+
+
   /* loop forever */
   for (;;) {
+    for (int i = 0; i < 1000; i++) {
 
-    GPIOB->BRR = 1<<13;
-    GPIOB->BSRR = 1<<14;
+      float f = (float)i / 1000.0f;
 
-    cyclesleep(0x084aa20);
+      uint8_t b = (int)(f * 8.0f);
+      uint32_t new_value = 0;
+      if (b & 1) new_value |= 1 << 13;
+      if (b & 2) new_value |= 1 << 14;
+      if (b & 4) new_value |= 1 << 15;
 
-    GPIOB->BRR = 1<<14;
-    GPIOB->BSRR = 1<<15;
+      // Clear bits that should not be set.
+      GPIOB->BRR = (~new_value) & OUTPUT_MASK;
+      GPIOB->BSRR = new_value & OUTPUT_MASK;
 
-    cyclesleep(0x084aa20);
+      cyclesleep(0x004aa20);
 
-    GPIOB->BRR = 1<<15;
-    GPIOB->BSRR = 1<<13;
+      GPIOB->BRR = 1<<13;
+      GPIOB->BSRR = 1<<14;
 
-    cyclesleep(0x084aa20);
+      // cyclesleep(0x084aa20);
+    }
   }
 }
 
