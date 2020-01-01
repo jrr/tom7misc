dd80388545a3341dddead601874c1185d733fd70
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-01-01 06:57:12 +0000

    Checkpoint this

---
+++ guitarchive/headers.h
@@ -0,0 +1,10 @@
+#ifndef __GUITARCHIVE_HEADERS_H
+#define __GUITARCHIVE_HEADERS_H
+
+#include <initializer_list>
+
+struct Headers {
+  static std::initializer_list<const char *> headers;
+};
+
+#endif
