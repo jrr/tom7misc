2d831aa9a823ab2c9039243887ad7b998cfd8cd0
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-01-05 16:54:03 +0000

    Lots more cleanup, extraction rules

--- trunk/guitarchive/guitarchive.cc
+++ trunk/guitarchive/guitarchive.cc
@@ -1,5 +1,6 @@
 // Code for cleaning and working with ASCII guitar tab files, e.g. from OLGA.
 
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <stdio.h>
@@ -19,9 +20,24 @@
 #include "headers.h"
 
 using namespace std;
-// using namespace re2;
 
-// file ends with "Set8" or "set8"
+// TODO: file ends with "Set8" or "set8"
+// TODO: file ends with enjoy!
+
+// TODO duplicate title, artist, etc.
+// Often these have small spelling or capitalization differences, too.
+
+// bytes 3 Jan 2020: 1829958389
+// bytes 4 Jan 2020: 1824308619
+
+// TODO: Remove duplicates!
+// TODO: Remove email header
+// TODO: Remove date header
+// TODO:
+// Band - Seether
+// Song - Cigarettes
+
+// \xc2\x97 is -
 
 static constexpr const char *DIRS[] = {
   "c:\\code\\electron-guitar\\tabscrape\\tabs",
@@ -43,7 +59,7 @@ struct Parser {
     return false;
   }
 
-  string FixMetadata(string contents) const {
+  string FixMetadata(const string &filename, string contents) const {
     RE2::GlobalReplace(&contents, bandspace,
 		       "Title: \\3\n"
 		       "Artist: \\1\n"
@@ -52,6 +68,18 @@ struct Parser {
     RE2::GlobalReplace(&contents, originalalbum, "Album: \\1\n");
     RE2::GlobalReplace(&contents, fromthealbum, "Album: \\1\n");
     RE2::GlobalReplace(&contents, authorartist, "Artist: \\1\n");
+
+    string title, artist;
+    if (Extract1(filename, &contents, &title, &artist) ||
+	Extract2(filename, &contents, &title, &artist) ||
+	Extract3(filename, &contents, &title, &artist)) {
+      contents = StringPrintf("Title: %s\n"
+			      "Artist: %s\n"
+			      "%s",
+			      title.c_str(),
+			      artist.c_str(),
+			      contents.c_str());
+    }
     return contents;
   }
 
@@ -76,9 +104,9 @@ struct Parser {
     return true;
   }
 
-  bool Extract(const string &filename, const string &contents,
+  bool Extract1(const string &filename, string *contents,
 	       string *title, string *artist) const {
-    re2::StringPiece cont(contents);
+    re2::StringPiece cont(*contents);
     string h1, h2;
     if (RE2::Consume(&cont, dashedheader, &h1, title, artist, &h2)) {
       if (h1.size() != h2.size()) return false;
@@ -88,6 +116,8 @@ struct Parser {
       string lartist = Util::lcase(*artist);
       if (ltitle.find("http:") != string::npos) return false;
       if (lartist.find("http:") != string::npos) return false;      
+      if (ltitle.find(" - ") != string::npos) return false;
+      if (lartist.find(" - ") != string::npos) return false;
       
       if (ltitle == "band name" || ltitle == "song name") return false;
       if (lartist == "band name" || lartist == "song name") return false;      
@@ -113,11 +143,314 @@ struct Parser {
 	  title->swap(*artist);
 	}
 	// XXX thresholds?
+
+	*contents = cont.as_string();
 	return true;
       }
     }
     return false;
   }
+
+  bool Extract2(const string &filename, string *contents,
+		string *title, string *artist) const {
+    re2::StringPiece cont(*contents);
+    if (RE2::Consume(&cont, byline, title, artist)) {
+      *title = Util::LoseWhiteR(*title);
+      *artist = Util::LoseWhiteR(*artist);
+
+      RE2::GlobalReplace(title, "[- ,\t]*$", "");
+      RE2::GlobalReplace(artist, "[- ,\t]*$", "");      
+
+      if (RE2::FullMatch(*title, ".* [Tt]ab$"))
+	return false;
+      if (RE2::FullMatch(*artist, ".* [Tt]ab$"))
+	return false;
+
+      if (RE2::FullMatch(*title, "^Tab:.*"))
+	return false;
+      if (RE2::FullMatch(*title, "^Song[ :].*"))
+	return false;
+      
+      string ltitle = Util::lcase(*title);
+      string lartist = Util::lcase(*artist);
+      if (ltitle.find("http:") != string::npos) return false;
+      if (lartist.find("http:") != string::npos) return false;      
+      if (ltitle.find(" - ") != string::npos) return false;
+      if (lartist.find(" - ") != string::npos) return false;
+      
+      if (ltitle == "band name" || ltitle == "song name") return false;
+      if (lartist == "band name" || lartist == "song name") return false;      
+
+      // Require corroboration with filenames.
+      // Here since we have "title by artist", it should really match that way.
+      string ftitle, fartist;
+      if (ExtractFile(filename, &ftitle, &fartist)) {
+	auto ReplaceUnder = [](string s) {
+	    for (char &c : s) {
+	      if (c == '_') c = ' ';
+	    }
+	    return s;
+	  };
+	ftitle = ReplaceUnder(Util::lcase(ftitle));
+	fartist = ReplaceUnder(Util::lcase(fartist));
+	
+	int same_loss = EditDistance::Distance(ftitle, ltitle) +
+	  EditDistance::Distance(fartist, lartist);
+	int diff_loss = EditDistance::Distance(ftitle, lartist) +
+	  EditDistance::Distance(fartist, ltitle);
+	if (diff_loss < same_loss) {
+	  return false;
+	}
+
+	if (same_loss > 10)
+	  return false;
+
+	*contents = cont.as_string();
+	return true;
+      }
+    }
+    return false;
+  }
+
+  bool Extract3(const string &filename, string *contents,
+		string *title, string *artist) const {
+    re2::StringPiece cont(*contents);
+    if (RE2::Consume(&cont, dashline, title, artist)) {
+      *title = Util::LoseWhiteR(*title);
+      *artist = Util::LoseWhiteR(*artist);
+
+      RE2::GlobalReplace(title, "[- ,|\t]*$", "");
+      RE2::GlobalReplace(artist, "[- ,|\t]*$", "");      
+
+      if (RE2::FullMatch(*title, ".* (?:[Bb]ass )?[Tt]ab$"))
+	return false;
+      if (RE2::FullMatch(*artist, ".* (?:[Bb]ass )?[Tt]ab$"))
+	return false;
+
+      if (RE2::FullMatch(*title, "^(?:[Bb]ass )Tab:.*"))
+	return false;
+      if (RE2::FullMatch(*title, "^Song[ :].*"))
+	return false;
+      
+      string ltitle = Util::lcase(*title);
+      string lartist = Util::lcase(*artist);
+      if (ltitle.find("http:") != string::npos) return false;
+      if (lartist.find("http:") != string::npos) return false;      
+      if (ltitle.find(" - ") != string::npos) return false;
+      if (lartist.find(" - ") != string::npos) return false;
+
+      // Field still has placeholder, or else "BAND - PINK FLOYD"
+      for (const char *no : {"band", "song", "band name", "song name",
+	    "artist", "title", "track"}) {
+	if (ltitle == no || lartist == no) return false;
+      }
+
+      // Require corroboration with filenames.
+      // Here since we have "title by artist", it should really match that way.
+      string ftitle, fartist;
+      if (ExtractFile(filename, &ftitle, &fartist)) {
+	auto ReplaceUnder = [](string s) {
+	    for (char &c : s) {
+	      if (c == '_') c = ' ';
+	    }
+	    return s;
+	  };
+	ftitle = ReplaceUnder(Util::lcase(ftitle));
+	fartist = ReplaceUnder(Util::lcase(fartist));
+	
+	int same_loss = EditDistance::Distance(ftitle, ltitle) +
+	  EditDistance::Distance(fartist, lartist);
+	int diff_loss = EditDistance::Distance(ftitle, lartist) +
+	  EditDistance::Distance(fartist, ltitle);
+	if (diff_loss < same_loss) {
+	  title->swap(*artist);
+	}
+	if (std::min(same_loss, diff_loss) > 10)
+	  return false;
+
+	*contents = cont.as_string();
+	return true;
+      }
+    }
+    return false;
+  }
+
+  // Extract Key: Value headers starting the file.
+  vector<pair<string, string>> ExtractHeaders(string *contents) const {
+    vector<pair<string, string>> hdrs;
+    re2::StringPiece cont(*contents);
+
+    do {
+      re2::StringPiece tmp = cont;
+      string key, value;
+      if (!RE2::Consume(&tmp, mischeader, &key, &value))
+	break;
+      
+      key = Util::LoseWhiteR(key);
+      value = Util::LoseWhiteR(value);
+
+      string lkey = Util::lcase(key);
+      int dashes = 0;
+      for (char c : value) if (c == '-') dashes++;
+      if (dashes > 6 ||
+	  value.empty() ||
+	  lkey == "http" ||
+	  (lkey.size() == 1 && lkey[0] >= 'a' && lkey[0] < 'g') || // note/chord names
+	  lkey == "am" ||
+	  lkey == "00" || lkey == "0" ||
+	  lkey.find("riff") == 0 ||
+	  lkey.find("chords") == 0 ||
+	  lkey.find("chorus") == 0 ||
+	  lkey.find("intro") == 0 ||
+	  lkey.find("guitar") == 0 ||
+	  lkey.find("verse") == 0 ||
+	  lkey.find("note") == 0) {
+	// These are not part of the headers, but rather introduce the content.
+	// Stop processing, and don't commit the 'consume' since it deletes leading
+	// whitespace.
+	break;
+      }
+
+      hdrs.emplace_back(key, value);
+      cont = tmp;
+    } while (true);
+    
+    *contents = cont.as_string();
+    return hdrs;
+  }
+
+  bool FilterHeaders(vector<pair<string, string>> *hdrs) const {
+    vector<pair<string, string>> ret;
+    ret.reserve(hdrs->size());
+    bool changed = false;
+    for (auto &p : *hdrs) {
+      string lkey = Util::lcase(p.first);
+      if (lkey == "received" &&
+	  p.second.find("SMTP") != string::npos) {
+	changed = true;
+	// p.first += " [DELETE]";
+	continue;
+      }
+      ret.push_back(std::move(p));
+    }
+    ret.swap(*hdrs);
+    return changed;
+  }
+
+  // Puts the headers in the canonical Title/Artist/Album order if possible,
+  // but otherwise leaves them intact. Must be exactly capital-t Title, etc.
+  void SortHeaders(vector<pair<string, string>> *hdrs) const {
+    std::stable_sort(hdrs->begin(),
+		     hdrs->end(),
+		     [](const pair<string, string> &a,
+			const pair<string, string> &b) {
+		       const string &keya = a.first;
+		       const string &keyb = b.first;
+		       if (keya == keyb)
+			 return false;
+		       
+		       if (keya == "Title") return true;
+		       if (keyb == "Title") return false;
+		       if (keya == "Artist") return true;
+		       if (keyb == "Artist") return false;
+		       if (keya == "Album") return true;
+		       if (keyb == "Album") return false;
+
+		       // Otherwise, keep same order.
+		       return false;
+		     });
+  }
+
+  void NormalizeHeaders(vector<pair<string, string>> *hdrs) const {
+    for (pair<string, string> &p : *hdrs) {
+      const string lkey = Util::lcase(p.first);
+      if (lkey == "title" || lkey == "song" || lkey == "songtitle" ||
+	  lkey == "titolo" || lkey == "track title" || lkey == "titile" ||
+	  lkey == "itle" || lkey == "track name" || lkey == "song tittle" ||
+	  lkey == "track title" || lkey == "tilte" || lkey == "titel" ||
+	  lkey == "titre" || lkey == "song title" || lkey == "song name" ||
+	  lkey == "tittle" || lkey == "titulo"
+	  ) {
+	p.first = "Title";
+      } else if (lkey == "artist" || lkey == "band" ||
+		 lkey == "band name" || lkey == "group" ||
+		 lkey == "artists" || lkey == "arist" ||
+		 lkey == "band/artist" || lkey == "artist/band" ||
+		 lkey == "artiste" || lkey == "band / artist" ||
+		 lkey == "artist/group" ||
+		 lkey == "artist name" || lkey == "artis" ||
+		 lkey == "song artist" || lkey == "auther/artist" ||
+		 lkey == "author/band" || lkey == "artista" ||
+		 lkey == "artisit" || lkey == "artsit" ||
+		 lkey == "artsist" || lkey == "autore" ||
+		 lkey == "banda" || lkey == "atrist" ||
+		 lkey == "artiest" || lkey == "groupe" ||
+		 lkey == "rtist" || lkey =="artist/ band") {
+	p.first = "Artist";
+      } else if (lkey == "album" || lkey == "albm" ||
+		 lkey == "ablum" || lkey == "albumn" ||
+		 lkey == "alblum" || lkey == "albulm" ||
+		 lkey == "cd" || lkey == "record" ||
+		 lkey == "album name" || lkey == "allbum" ||
+		 lkey == "album/ep" ||
+		 lkey == "taken from the album" ||
+		 lkey == "from the album" ||
+		 lkey == "album title" || lkey == "from album") {
+	p.first = "Album";
+      } else if (lkey == "tuning" || lkey == "tunning" ||
+		 lkey == "tuneing" || lkey == "tuned" ||
+		 lkey == "tunging" || lkey == "tune") {
+	p.first = "Tuning";
+      } else if (lkey == "capo") {
+	p.first = "Capo";
+      } else if (lkey == "tabbed by" || lkey == "tabbedby" ||
+		 lkey == "tabed by" || lkey == "tabber" ||
+		 lkey == "taber" || lkey == "tabbe by" ||
+		 lkey == "tab by" || lkey == "tabs by" ||
+		 lkey == "tab author" || lkey == "tabbed out by" ||
+		 lkey == "transcribe by" ||
+		 lkey == "transcribed by" || lkey == "transcriber") {
+	p.first = "Tabbed by";
+      } else if (lkey == "email" || lkey == "e-mail" || lkey == "mail" ||
+		 lkey == "email address" || lkey == "e-mail address" ||
+		 lkey == "tabbers email" || lkey == "my email" ||
+		 lkey == "email add" || lkey == "emil" ||
+		 lkey == "contact email") {
+	p.first = "E-mail";
+      } else if (lkey == "date") {
+	p.first = "Date";
+      } else if (lkey == "key") {
+	p.first = "Key";
+      } else if (lkey == "year") {
+	p.first = "Year";
+      } else if (lkey == "subject") {
+	p.first = "Subject";
+      }
+      p.second = Util::losewhitel(p.second);
+      p.second = Util::LoseWhiteR(p.second);
+    }
+  }
+  
+  string FixHeaders(string contents) const {
+    string out;
+    out.reserve(contents.size());
+
+    vector<pair<string, string>> hdrs = ExtractHeaders(&contents);
+    FilterHeaders(&hdrs);
+    NormalizeHeaders(&hdrs);
+    SortHeaders(&hdrs);
+    for (const auto &p : hdrs) {
+      out += StringPrintf("%s: %s\n", p.first.c_str(), p.second.c_str());
+    }
+    out += contents;
+    return out;
+  }
+  
+  RE2 mischeader{
+    // Allow totally blank lines.
+    "\n*"
+    " *([A-Za-z0-9'/ ]+) *: *(.*)\n"};
+  
   
   // Preferred explicit form at the beginning of the file.
   RE2 explicit_meta{"Title: (.*)\n"
@@ -133,14 +466,17 @@ struct Parser {
 
   RE2 dashedheader{"^"
 		   "------------(-*)\n"
-		   "[ \t]**([A-Za-z0-9].*)[ \t]* - [ \t]*([A-Za-z0-9].*)\n"
+		   "[ \t]*([A-Za-z0-9].*)[ \t]* - [ \t]*([A-Za-z0-9].*)\n"
 		   "------------(-*)\n", MultiLine()};
-
+  RE2 byline{"^[ \t]*([A-Za-z0-9].*)[ \t]+[Bb]y[ \t]+([A-Za-z0-9].*)\n", MultiLine()};
+  RE2 dashline{"^[ \t]*([A-Za-z0-9].*)[ \t]+-[ \t]+([A-Za-z0-9].*)\n", MultiLine()};
+	     
+  
   RE2 remove_ver{"(?:_ver[0-9]+)?$"};
+  // TODO: Support OLGA dir as well
   RE2 tabfilename{"d:\\\\temp\\\\tabs[/\\\\].[/\\\\]([^/\\\\]*)[/\\\\]([^/\\\\]*)"
 		  "_(?:b?tab|crd|lyr)"
 		  ".txt"};
-  
 };
 
 
@@ -416,7 +752,10 @@ string FixBadEncoding(const string &s) {
   r = Util::Replace(r, "\xc2\x92", "'");
   r = Util::Replace(r, "\xc2\x93", "\"");
   r = Util::Replace(r, "\xc2\x94", "\"");
+  // These may be en and em dash. They often appear randomly in tablature,
+  // almost like watermarks.
   r = Util::Replace(r, "\xc2\x96", "-");
+  r = Util::Replace(r, "\xc2\x97", "-");
   // Seems to be a problem with the 'tabs' scrape.
   // r = Util::Replace(r, "\\\\\" ", "\"");
   // And for the same at EOL after trailing whitespace was removed.
@@ -523,6 +862,8 @@ int main(int argc, char **argv) {
 		      row.second.find("scholarship, or research") != string::npos ||
 		      row.second.find("Have fun, DAIRYBEAT on") != string::npos ||
 		      row.second.find("and has a specific punishment") != string::npos ||
+		      // XXX
+		      HasNonAscii(row.second) ||
 		      row.second.find("in an unauthorized application,") != string::npos) {
 		    MutexLock ml(&m);
 		    todo.push_back(row.first);
@@ -562,7 +903,8 @@ int main(int argc, char **argv) {
     fflush(stdout);
   }
 
-  {
+
+  if (false) {
     Parser p;
     std::mutex m;
     int has_extraction = 0;
@@ -570,7 +912,8 @@ int main(int argc, char **argv) {
     ParallelApp(files,
 		[&p, &m, &has_extraction, &examples](const pair<string, string> &row) {
 		  string title, artist;
-		  if (p.Extract(row.first, row.second, &title, &artist)) {
+		  string contents = row.second;
+		  if (p.Extract3(row.first, &contents, &title, &artist)) {
 		    MutexLock ml(&m);
 		    examples.push_back(
 			StringPrintf("[%s] by [%s] (%s)\n",
@@ -587,7 +930,99 @@ int main(int argc, char **argv) {
       fprintf(f, "%s", s.c_str());
     fclose(f);
   }
-    
+
+  if (false) {
+    Parser p;
+    std::mutex m;
+    int has_extraction = 0;
+    vector<string> examples;
+    ParallelApp(files,
+		[&p, &m, &has_extraction, &examples](const pair<string, string> &row) {
+		  string title, artist;
+		  string contents = row.second;
+		  vector<pair<string, string>> orig_hdrs = p.ExtractHeaders(&contents);
+		  if (!orig_hdrs.empty()) {
+		    vector<pair<string, string>> hdrs = orig_hdrs;
+		    p.FilterHeaders(&hdrs);
+		    p.NormalizeHeaders(&hdrs);
+		    p.SortHeaders(&hdrs);
+
+		    if (orig_hdrs != hdrs) {
+		      string ex = row.first + (string)"\n";
+		      for (const auto &p : hdrs) {
+			ex += StringPrintf("  [%s] = [%s]\n", p.first.c_str(), p.second.c_str());
+		      }
+
+		      MutexLock ml(&m);
+		      examples.push_back(std::move(ex));
+		      has_extraction++;
+		    }
+		  }
+		},
+		32);
+    printf("Total with extractions: %d\n", has_extraction);
+    fflush(stdout);
+    FILE *f = fopen("extractions.txt", "wb");
+    for (const string &s : examples)
+      fprintf(f, "%s", s.c_str());
+    fclose(f);
+  }
+
+  if (true) {
+    Parser p;
+    std::mutex m;
+    std::unordered_map<string, tuple<int, string, string>> keys;
+    ParallelApp(files,
+		[&p, &m, &keys](const pair<string, string> &row) {
+		  string title, artist;
+		  string contents = row.second;
+		  vector<pair<string, string>> orig_hdrs = p.ExtractHeaders(&contents);
+		  if (!orig_hdrs.empty()) {
+		    vector<pair<string, string>> hdrs = orig_hdrs;
+		    p.FilterHeaders(&hdrs);
+		    p.NormalizeHeaders(&hdrs);
+		    p.SortHeaders(&hdrs);
+
+		    MutexLock ml(&m);
+		    for (const auto &kv : hdrs) {
+		      auto &v = keys[kv.first];
+		      std::get<0>(v)++;
+		      if (std::get<1>(v).empty()) std::get<1>(v) = kv.second;
+		      if (std::get<2>(v).empty()) std::get<2>(v) = row.first;
+		    }
+		  }
+		},
+		32);
+    printf("Total different keys: %d\n", (int)keys.size());
+    fflush(stdout);
+
+    vector<pair<int, string>> sorted;
+    sorted.reserve(keys.size());
+    for (const auto &r : keys) {
+      const string &key = r.first;
+      int count;
+      string value, file;
+      std::tie(count, value, file) = r.second;
+      sorted.emplace_back(count,
+			  StringPrintf("[%s] = [%s] %s",
+				       key.c_str(), value.c_str(), file.c_str()));
+    }
+
+    std::sort(sorted.begin(), sorted.end(),
+	      [](const pair<int, string> &a,
+		 const pair<int, string> &b) {
+		return a.first > b.first;
+	      });
+
+    FILE *f = fopen("keys.txt", "wb");    
+    for (const auto &r : sorted) {
+      fprintf(f, "%d %s\n", r.first, r.second.c_str());
+    }
+    fclose(f);
+  }
+
+  // return 0;
+  
   {
     Parser p;
     std::mutex m;
@@ -598,7 +1033,8 @@ int main(int argc, char **argv) {
 		  c = FixBadEncoding(c);
 		  c = StripCR(c);
 		  c = StripHeader(c);
-		  c = p.FixMetadata(c);
+		  c = p.FixMetadata(row.first, c);
+		  c = p.FixHeaders(c);
 		  // c = StripFooter(c);
 		  /*
 		    if (!c.empty() && c[0] == ' ') {
