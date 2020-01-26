ada867ba2e514fa126310a2359a20d526aff3b85
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-01-26 05:16:41 +0000

    Split onewire into its own file. Add database-based temp details

---
+++ tempo/onewire.cc
@@ -0,0 +1,52 @@
+
+#include "onewire.h"
+
+#include <cstdint>
+
+#include "../cc-lib/base/logging.h"
+#include "../cc-lib/util.h"
+
+using uint32 = uint32_t;
+using namespace std;
+
+OneWire::OneWire() {
+  vector<string> files = Util::ListFiles(dir);
+  for (const string &file : files) {
+    const string fullpath = dir + "/" + file + "/w1_slave";
+    printf("Trying %s...\n", fullpath.c_str());
+    string testread = Util::ReadFile(fullpath);
+    if (!testread.empty()) {
+      CHECK(probes.find(file) == probes.end())
+	<< file << " duplicate?";
+      probes[file].fullpath = fullpath;
+      printf("  ... OK\n");
+    } else {
+      printf("  ... (failed -- might be a master?)\n");
+    }
+  }
+
+  printf("Found %d probe(s).\n", (int)probes.size());
+}
+
+bool OneWire::Probe::Temperature(uint32 *microdeg_c) {
+  // This is coming from the /proc filesystem, but still,
+  // this is a very bizarre format. Two lines, like so:
+  // 1c 01 4b 46 7f ff 04 10 e8 : crc=e8 YES
+  // 1c 01 4b 46 7f ff 04 10 e8 t=17750
+  // The hex dump is literally there in the file, and both
+  // lines seem to always be the same (so I guess this is
+  // like the raw data--but I don't see any way to get the
+  // data itself; just this dump!). The encoding is not
+  // obvious. Other than the last byte being the CRC, there
+  // aren't two bytes there representing 17750 (0x4556).
+  // But 17750 is 17.750 degrees C.
+  string data = Util::ReadFile(fullpath);
+  if (data.empty()) return false;
+  if (data.find("YES") == string::npos) return false;
+  size_t te = data.find("t=");
+  if (te == string::npos) return false;
+  const uint32 reading = atoi(&data[te + 2]);
+  last_reading = reading;
+  *microdeg_c = reading;
+  return true;
+}
