5ddc0dc764480867172bcf6efb4316a711029885
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-02-09 15:18:34 +0000

    Add wu's algorithm to lines templates

--- cc-lib/lines.h
+++ cc-lib/lines.h
@@ -1,25 +1,31 @@
+// Generate lines using some classic graphics algorithms.
+// Templated on integer/float types since sometimes it is helpful
+// to use lower-precision types in embedded applications.
+
+#ifndef __CC_LIB_LINES_H
+#define __CC_LIB_LINES_H
+
+#include <utility>
+#include <cmath>
+#include <tuple>
+#include <type_traits>
+
 // Generate lines with Bresenham's algorithm. Use like this:
-//
-// #include <utility>
 //
 // /* Draw a line from (3, 4) to (-7, 8). */
 // for (const std::pair<int, int> point : Line<int>{3, 4, -7, 8}) {
 //   int x = point.first, y = point.second;
 //   drawpixel(x, y);
 // }
-
-#ifndef __CC_LIB_LINES_H
-#define __CC_LIB_LINES_H
-
-#include <utility>
-#include <type_traits>
-
+//
+// C++17:
+// for (auto [x, y] : Line<int>{3, 4, -7, 8})
+//   drawpixel(x, y);
 template<class Int>
 class Line {
  public:
   static_assert(std::is_integral<Int>::value, "Line<T> requires integral T.");
   Line(Int x0, Int y0, Int x1, Int y1);
-  bool Next(Int *x, Int *y);
 
   // This iterator is only designed for ranged-for loops; the operators
   // may have counter-intuitive behavior.
@@ -44,6 +50,26 @@ class Line {
   Int stepx, stepy;
   Int start_frac;
 };
+
+// Anti-aliased line using Wu's algorithm. The pixel to plot is
+// accompanied by a brightness fraction in [0, 1].
+// This is not suited for an iterator-based interface since it treats
+// the endpoints separately, and draws two pixels per iteration.
+// Not possible to return early.
+//
+// /* Draw a line from (1.0, 3.1) to (-7, 8.5), using single-precision
+//    floats (deduced from args) and int output. */
+// LineAA::Draw<int>(1.0f, 3.1f, -7.0f, 8.5f,
+//                   [](int x, int y, float f) {
+//                       blendpixel(x, y, f * 255.0f);
+//                   });
+class LineAA {
+public:
+  template<class Int, class Float, class Fn>
+  static void Draw(Float x0, Float y0, Float x1, Float y1, Fn drawpixel);
+};
+
+
 
 // Template implementations follow.
 
@@ -125,5 +151,84 @@ void Line<Int>::iterator::operator ++() {
     frac += parent.dx;
   }
 }
+
+
+
+template<class Int, class Float, class Fn>
+void LineAA::Draw(Float x0, Float y0, Float x1, Float y1, Fn drawpixel) {
+  static_assert(std::is_integral<Int>::value,
+		"LineAA<T,F> requires integral T.");
+  static_assert(std::is_floating_point<Float>::value,
+		"LineAA<T,F> requires floating-point F.");
+
+  // floor and round are each overloaded on float and double.
+  auto ipart = [](Float x) -> Int { return Int(std::floor(x)); };
+  auto round = [](Float x) -> Float { return std::round(x); };
+  auto fpart = [](Float x) -> Float { return x - std::floor(x); };
+  auto rfpart = [fpart](Float x) -> Float { return Float(1.0f) - fpart(x); };
+ 
+  const bool steep = std::abs(y1 - y0) > std::abs(x1 - x0);
+  if (steep) {
+    std::swap(x0,y0);
+    std::swap(x1,y1);
+  }
+  if (x0 > x1) {
+    std::swap(x0,x1);
+    std::swap(y0,y1);
+  }
+ 
+  const Float dx = x1 - x0;
+  const Float dy = y1 - y0;
+  const Float gradient = (dx == 0) ? 1 : dy / dx;
+ 
+  Int xpx11;
+  Float intery;
+  {
+    const Float xend = round(x0);
+    const Float yend = y0 + gradient * (xend - x0);
+    const Float xgap = rfpart(x0 + 0.5);
+    xpx11 = Int(xend);
+    const Int ypx11 = ipart(yend);
+    if (steep) {
+      drawpixel(ypx11, xpx11, rfpart(yend) * xgap);
+      drawpixel(ypx11 + 1, xpx11, fpart(yend) * xgap);
+    } else {
+      drawpixel(xpx11, ypx11, rfpart(yend) * xgap);
+      drawpixel(xpx11, ypx11 + 1, fpart(yend) * xgap);
+    }
+    intery = yend + gradient;
+  }
+ 
+  Int xpx12;
+  {
+    const Float xend = round(x1);
+    const Float yend = y1 + gradient * (xend - x1);
+    const Float xgap = rfpart(x1 + 0.5);
+    xpx12 = Int(xend);
+    const Int ypx12 = ipart(yend);
+    if (steep) {
+      drawpixel(ypx12, xpx12, rfpart(yend) * xgap);
+      drawpixel(ypx12 + 1, xpx12, fpart(yend) * xgap);
+    } else {
+      drawpixel(xpx12, ypx12, rfpart(yend) * xgap);
+      drawpixel(xpx12, ypx12 + 1, fpart(yend) * xgap);
+    }
+  }
+ 
+  if (steep) {
+    for (Int x = xpx11 + 1; x < xpx12; x++) {
+      drawpixel(ipart(intery), x, rfpart(intery));
+      drawpixel(ipart(intery) + 1, x, fpart(intery));
+      intery += gradient;
+    }
+  } else {
+    for (Int x = xpx11 + 1; x < xpx12; x++) {
+      drawpixel(x, ipart(intery), rfpart(intery));
+      drawpixel(x, ipart(intery) + 1, fpart(intery));
+      intery += gradient;
+    }
+  }
+}
+
 
 #endif
--- cc-lib/lines_test.cc
+++ cc-lib/lines_test.cc
@@ -6,9 +6,7 @@
 #include "arcfour.h"
 #include "randutil.h"
 
-int main() {
-  ArcFour rc{"lines_test"};
-
+static void TestBresenham() {
   for (const std::pair<int, int> point : Line<int>{0, 0, 5, 5}) {
     printf("%d,%d  ", point.first, point.second);
   }
@@ -24,10 +22,26 @@ int main() {
   }
 
   printf("\n--\n");
-  for (const std::pair<int, int> point : Line<int>{4, 5, 0, 0}) {
-    printf("%d,%d  ", point.first, point.second);
+  for (auto [x, y] : Line<int>{4, 5, 0, 0}) {
+    printf("%d,%d  ", x, y);
   }
 
   printf("\n--\n");
+}
+
+static void TestWu() {
+  auto Plot = [](int x, int y, float f) {
+      printf("%d,%d %.2f  ", x, y, f);
+    };
+  LineAA::Draw<int>(0.5f, 4.0f, 2.5f, 3.0f, Plot);
+  printf("\n---\n");
+}
+
+int main() {
+  // ArcFour rc{"lines_test"};
+
+  TestBresenham();
+  TestWu();
+  
   return 0;
 }
