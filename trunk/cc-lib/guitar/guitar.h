a04ca196e2af1f6869c678b127c08972e7ddec54
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-04-25 16:55:45 +0000

    Add sus2sus4 chords.

--- trunk/cc-lib/guitar/guitar.h
+++ trunk/cc-lib/guitar/guitar.h
@@ -64,20 +64,16 @@ struct Guitar {
     "C","C#","D","Eb","E","F","F#","G","Ab","A","Bb","B",
   };
 
-  static constexpr int NUM_SUFFIXES = 64;
+  static constexpr int NUM_SUFFIXES = 65;
   static constexpr std::array<const char *, NUM_SUFFIXES> SUFFIXES = {
-    // aka. "major"
-    "",
-    // aka. "minor"
-    "m",
-    "dim", "dim7", "sus2", "sus4", "7sus4", "alt", "aug", "5", "6",
+    "", // aka. "major"
+    "m", // aka. "minor"
+    "dim", "dim7", "sus2", "sus4", "sus2sus4", "7sus4", "alt", "aug", "5", "6",
     "69", // aka. "6/9", "6add9"
-    "7",
-    "7b5", "aug7", "9", "9b5", "aug9", "7b9", "7#9", "11", "9#11", "13",
+    "7", "7b5", "aug7", "9", "9b5", "aug9", "7b9", "7#9", "11", "9#11", "13",
     "maj7", "maj7b5", "maj7#5", "maj9", "maj11", "maj13", "m6",
     "m69", // aka. "m6/9", "m6add9"
-    "m7",
-    "m7b5", "m9", "m11", "mmaj7", "mmaj7b5", "mmaj9", "mmaj11", "add9",
+    "m7", "m7b5", "m9", "m11", "mmaj7", "mmaj7b5", "mmaj9", "mmaj11", "add9",
     "madd9", "7/G", "/E", "/F", "/F#", "/G", "/G#", "/A", "/Bb", "/B", "/C",
     "/C#", "m/B", "m/C", "m/C#", "/D", "m/D", "/D#", "m/D#", "m/E", "m/F",
     "m/F#", "m/G", "m/G#",
