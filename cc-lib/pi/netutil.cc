dbe1132cf01a2df2cd3f6e23e4109b04a811b10a
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-05-20 02:28:17 +0000

    Add MAC addresses to this utility too. They are often useful for identifying the device.

--- cc-lib/pi/netutil.cc
+++ cc-lib/pi/netutil.cc
@@ -2,6 +2,7 @@
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <linux/if_packet.h>
 #include <netdb.h>
 #include <ifaddrs.h>
 #include <stdio.h>
@@ -16,6 +17,7 @@
 
 using namespace std;
 using ip4 = NetUtil::ip4;
+using mac = NetUtil::mac;
 
 // based on the manpage for getifaddrs.
 std::map<std::string, ip4> NetUtil::GetIP4Interfaces() {
@@ -54,14 +56,68 @@ std::map<std::string, ip4> NetUtil::GetIP4Interfaces() {
   return ret;
 }
 
-std::optional<ip4> NetUtil::BestGuessIPAddress() {
+std::map<std::string, mac> NetUtil::GetMACAddresses() {
+  struct ifaddrs *ifaddr = nullptr;
+  if (getifaddrs(&ifaddr) == -1)
+    return {};
+
+  map<string, mac> ret;
+  for (struct ifaddrs *ifa = ifaddr;
+       ifa != nullptr;
+       ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr == nullptr)
+      continue;
+
+    switch (ifa->ifa_addr->sa_family) {
+    case AF_PACKET: {
+      const char *name = ifa->ifa_name;
+      // sockaddr_ll is "link level"
+      const sockaddr_ll *ll = (const sockaddr_ll *)ifa->ifa_addr;
+      const uint8_t *mac = &ll->sll_addr[0];
+      ret.emplace(name, make_tuple(mac[0], mac[1], mac[2],
+				   mac[3], mac[4], mac[5]));
+      break;
+    }
+    default:
+      continue;
+    }
+  }
+
+  freeifaddrs(ifaddr);
+  return ret;
+}
+
+
+std::optional<std::pair<ip4, mac>> NetUtil::BestGuessIPWithMAC() {
   // TODO: Better logic here.
   // Avoid 127.0.0.1.
   // Prefer interfaces named "eth0", "eth1", "wlan0", etc.
   // Prefer "real" internet addresses over 10.* and 192.168.*
-  map<string, ip4> ifaces = GetIP4Interfaces();
+  map<string, ip4> ips = GetIP4Interfaces();
+  map<string, mac> macs = GetMACAddresses();
+
+  map<string, std::pair<ip4, mac>> ifaces;
+  for (const auto &[name, ip] : ips) {
+    auto it = macs.find(name);
+    if (it != macs.end()) {
+      ifaces[name] = make_pair(ip, it->second);
+    }
+  }
+  
   if (ifaces.find("eth0") != ifaces.end()) return {ifaces["eth0"]};
   if (ifaces.find("wlan0") != ifaces.end()) return {ifaces["wlan0"]};
   if (ifaces.begin() != ifaces.end()) return {ifaces.begin()->second};
   return {};
 }
+
+std::optional<ip4> NetUtil::BestGuessIPAddress() {
+  auto opt = BestGuessIPWithMAC();
+  if (opt) return {std::get<0>(*opt)};
+
+  // Don't fail (because we didn't find a MAC) if there are ANY
+  // interfaces with IP addresses.
+  map<string, ip4> ips = GetIP4Interfaces();
+  if (ips.empty()) return {};
+  return {ips.begin()->second};
+}
+
--- cc-lib/pi/netutil.h
+++ cc-lib/pi/netutil.h
@@ -10,13 +10,17 @@
 struct NetUtil {
 
   using ip4 = std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>;
+  using mac = std::tuple<uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t>;
   
   static std::map<std::string, ip4> GetIP4Interfaces();
-
+  static std::map<std::string, mac> GetMACAddresses();
+  
   // Like above, but tries to figure out which one is our "real"
   // external-facing IP address.
   static std::optional<ip4> BestGuessIPAddress();
-
+  // Same, preferring an ethernet address with a MAC address.
+  static std::optional<std::pair<ip4, mac>> BestGuessIPWithMAC();
+  
 };
 
 #endif
