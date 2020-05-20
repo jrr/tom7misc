5391af2b831fede661c46d8d67be317fcdb909fb
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-05-20 02:28:17 +0000

    Add MAC addresses to this utility too. They are often useful for identifying the device.

--- trunk/cc-lib/pi/netutil_test.cc
+++ trunk/cc-lib/pi/netutil_test.cc
@@ -3,6 +3,7 @@
 
 using namespace std;
 using ip4 = NetUtil::ip4;
+using mac = NetUtil::mac;
 
 int main(int argc, char **argv) {
   for (const auto [name, ip] : NetUtil::GetIP4Interfaces()) {
@@ -10,7 +11,12 @@ int main(int argc, char **argv) {
     printf("%s: %d.%d.%d.%d\n", name.c_str(), a, b, c, d);
   }
 
-  printf("Best guess: ");
+  for (const auto [name, mac] : NetUtil::GetMACAddresses()) {
+    auto [a, b, c, d, e, f] = mac;
+    printf("%s: %02x:%02x:%02x:%02x:%02x:%02x\n", name.c_str(), a, b, c, d, e, f);
+  }
+  
+  printf("Best guess IP (only): ");
   optional<ip4> ip = NetUtil::BestGuessIPAddress();
   if (ip) {
     auto [a, b, c, d] = *ip;
@@ -19,6 +25,17 @@ int main(int argc, char **argv) {
     printf("(nullopt!)\n");
   }
 
+  printf("Best guess IP with MAC: ");
+  optional<pair<ip4, mac>> opt = NetUtil::BestGuessIPWithMAC();
+  if (opt) {
+    auto [a, b, c, d] = std::get<0>(*opt);
+    auto [e, f, g, h, i, j] = std::get<1>(*opt);
+    printf("%d.%d.%d.%d  %02x:%02x:%02x:%02x:%02x:%02x\n", a, b, c, d,
+	   e, f, g, h, i, j);
+  } else {
+    printf("(nullopt!)\n");
+  }
+  
   // This test can't really fail (other than crashing); you have to
   // inspect the output.
   return 0;
