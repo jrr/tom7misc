5c845a1ea10a541e8d38f2e800503344ee7329aa
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-06-10 04:22:26 +0000

    Clean up webserver a bit, and move it to a better filename. Add Params() routine for request and notes

--- trunk/cc-lib/webserver.h
+++ trunk/cc-lib/webserver.h
@@ -1,11 +1,12 @@
-#ifndef __CCLIB_WEB_H
-#define __CCLIB_WEB_H
+#ifndef __CCLIB_WEBSERVER_H
+#define __CCLIB_WEBSERVER_H
 
 #include <cstdint>
 #include <string>
 #include <vector>
 #include <utility>
 #include <functional>
+#include <optional>
 
 struct WebServer {
   static WebServer *Create();
@@ -20,8 +21,6 @@ struct WebServer {
   // for the path in a request. If a handler's prefix matches the request,
   // the handler function is called on the request and must return a response.
   //
-  // The handler function can 
-  //
   // To do custom dispatching, just register the prefix "/" which starts all
   // valid requests.
   struct Request;
@@ -34,8 +33,7 @@ struct WebServer {
   virtual Handler GetStatsHandler() const = 0;
   // TODO: Static file handler, directory listing, that kinda stuff.
 
-  /* You'll look directly at this struct to handle HTTP requests. It's initialized
-     by setting everything to 0 */
+  // An incoming request.
   struct Request {
     /* HTTP method (GET, POST, PUT, ...) */
     std::string method;
@@ -43,28 +41,40 @@ struct WebServer {
     std::string version;
     /* HTTP path/URI ( /index.html?name=Forrest%20Heller ) */
     std::string path;
-    /* null-terminated HTTP path/URI that has been %-unescaped. Used for a file serving */
-    char pathDecoded[1024];
-    size_t pathDecodedLength = 0;
 
     // Content length as sent by the client. 
     int content_length = 0;
     /* the request body. Used for POST forms and JSON blobs */
     std::string body;
-    /* HTTP request headers - use headerInRequest to find the header you're looking for. */
+    /* HTTP request headers - use headerInRequest to find the header
+       you're looking for. */
     std::vector<std::pair<std::string, std::string>> headers;
     /* warning: request line strings truncated? */
-    bool bodyTruncated = false;
+    bool body_truncated = false;
 
     // Utilities
     // Find a header's value (case insensitive) or return nullptr.
-    const std::string *GetHeader(const std::string &name);
+    const std::string *GetHeader(const std::string &name) const;
+    // Decoded URL parameters in the order they appear in the URL.
+    // A parameter can appear more than once. Unparseable parameters
+    // may be discarded.
+    std::vector<std::pair<std::string, std::string>> Params() const;
+
+    // TODO: Implement these conveinences...
+    // Get the first occurrence of the url param and decode it,
+    // or return nullopt if not present.
+    // std::optional<std::string> StringURLParam(const std::string &name) const;
+    // Get the first occurrence of the url param, decode and parse
+    // it as an integer, or return nullopt otherwise.
+    // std::optional<int64_t> IntURLParam(const std::string &name) const ;
   };
 
-  /* You create one of these for the server to send. Use one of the responseAlloc functions.
-     You can fill out the body field using the heapString* functions. You can also specify a
-     filenameToSend which will be sent using regular file streaming. This is so you don't have
-     to load the entire file into memory all at once to send it. */
+  /* You create one of these for the server to send. Use one of the
+     responseAlloc functions. You can fill out the body field using
+     the heapString* functions. You can also specify a filenameToSend
+     which will be sent using regular file streaming. This is so you
+     don't have to load the entire file into memory all at once to
+     send it. */
   struct Response {
     // e.g. 404
     int code = 0;
@@ -92,9 +102,11 @@ struct WebServer {
   
   // Utilities.
 
-  // Returns e.g. "text/html; charset=UTF-8" for a file whose name ends in ".html".
-  // This is quite incomplete; beyond simple stuff try apache2's module?
-  static std::string GuessMIMEType(const std::string &filename, const std::string &contents);
+  // Returns e.g. "text/html; charset=UTF-8" for a file whose name
+  // ends in ".html". This is quite incomplete; beyond simple stuff
+  // try apache2's module?
+  static std::string GuessMIMEType(const std::string &filename,
+				   const std::string &contents);
   // Basic, slow: Replace " < & etc. with HTML entities.
   static std::string HTMLEscape(const std::string &input);
   
