74996bd24d5175bc9ede801f70a46127275a9680
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-09-19 23:03:43 +0000

    Make solution not depend on util so that it's more hermetic (for server). These functions are terrible and should be replaced anyway

--- trunk/escapex/solution.cc
+++ trunk/escapex/solution.cc
@@ -2,22 +2,45 @@
 #include "solution.h"
 
 #include <string>
+#include <cstdint>
 
 #include "rle.h"
-#include "util.h"
 
+using uint32 = uint32_t;
+using uint8 = uint8_t;
 using namespace std;
 
+static string BigEndian32(int i) {
+  string s = "    ";
+  s[0] = 255 & (i >> 24);
+  s[1] = 255 & (i >> 16);
+  s[2] = 255 & (i >> 8);
+  s[3] = 255 &  i;
+  return s;
+}
+
+static uint32 ReadBigEndian32(const string &s, int idx) {
+  auto Byte32 = [&s](int idx) -> uint32 {
+      uint8 b = s[idx];
+      return (uint32)b;
+    };
+  return (Byte32(0) << 24) |
+    (Byte32(1) << 16) |
+    (Byte32(2) << 8) |
+    Byte32(3);
+}
+
 string Solution::ToString() const {
-  return sizes(Length()) + EscapeRLE::Encode(Length(), (int *)dirs.data());
+  return BigEndian32(Length()) + EscapeRLE::Encode(Length(), (int *)dirs.data());
 }
 
 // static
 bool Solution::FromString(const string &s, Solution *sol) {
-  unsigned int idx = 0;
   if (s.length() < 4) return false;
-  int len = shout(4, s, idx);
+  int len = ReadBigEndian32(s, 0);
+  if (len < 0) return false;
 
+  unsigned int idx = 4;
   dir *dd = EscapeRLE::Decode(s, idx, len);
 
   if (!dd) return false;
