bf929a47a75648c3e683cf81349a5967bb773857
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-09-19 18:34:56 +0000

    Move bitbuffer out of util, as foretold by the bards. It conflicts with escape's copy and should do this anyway

---
+++ cc-lib/bitbuffer.cc
@@ -0,0 +1,68 @@
+#include "bitbuffer.h"
+
+#include <string>
+
+using namespace std;
+
+int BitBuffer::ceil(int bits) {
+  return (bits >> 3) + !!(bits & 7);
+}
+
+
+bool BitBuffer::nbits(const string &s, int n, int &idx, unsigned int &out) {
+# define NTHBIT(x) !! (s[(x) >> 3] & (1 << (7 - ((x) & 7))))
+
+  out = 0;
+
+  while (n--) {
+    out <<= 1;
+    /* check bounds */
+    if ((unsigned)(idx >> 3) >= s.length()) return false;
+    out |= NTHBIT(idx);
+    idx ++;
+  }
+  return true;
+
+# undef NTHBIT
+}
+
+string BitBuffer::getstring() {
+  int n = ceil(bits);
+  if (data) return string((char *)data, n);
+  else return "";
+}
+
+void BitBuffer::writebits(int n, unsigned int b) {
+  /* assumes position already holds 0 */
+# define WRITEBIT(x, v) data[(x) >> 3] |= ((!!v) << (7 - ((x) & 7)))
+
+  /* printf("writebits(%d, %d)\n", n, b); */
+
+  for (int i = 0; i < n; i ++) {
+    int bytes_needed = ceil(bits + 1);
+
+    /* allocate more */
+    if (bytes_needed > size) {
+      int nsize = (size + 1) * 2;
+      unsigned char *tmp =
+	(unsigned char *) malloc(nsize * sizeof (unsigned char));
+      if (!tmp) abort();
+      memset(tmp, 0, nsize);
+      memcpy(tmp, data, size);
+      free(data);
+      data = tmp;
+      size = nsize;
+    }
+
+    int bit = !!(b & (1 << (n - (i + 1))));
+    /* printf("  write %d at %d\n", bit, bits); */
+    WRITEBIT(bits, bit);
+    bits++;
+  }
+
+# undef WRITEBIT
+}
+
+BitBuffer:~BitBuffer() {
+  free(data);
+}
