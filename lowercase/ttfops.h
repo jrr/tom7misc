a4d287c2178c85430f1424aa2c1b510166e1b372
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-12-13 16:00:37 +0000

    OK, can make it through the whole data set now...

--- lowercase/ttfops.h
+++ lowercase/ttfops.h
@@ -15,17 +15,37 @@ struct TTFOps {
   // Translation for each character is not returned.
   // Caller should do some thresholding on score, since we produce
   // a result whenever an alignment is possible, even if it is bad.
+  //
+  // This requires the characters to be structurally equivalent.
+  // BitmapDifference is a slower but more robust check for the
+  // *appearance* being equivalent.
   static std::optional<std::tuple<double, double, double>>
   GetSameCase(const TTF &ttf);
 
-  #if 0
-  // XXX do in UI to test
-  double BitmapDifference(const TTF &ttf,
-			  char c1, char c2,
-			  float xscale, yscale, ...);
+  // Render the two characters to bitmaps at the given scale, and then
+  // return the difference as the fraction of pixels in the second
+  // bitmap that are the same in the first (will be in [0, 1]).
+  static double CharBitmapDifference(const TTF &ttf,
+				     int c1, int c2,
+				     // Determines the base bitmap size for both
+				     // characters. c1 is unstretched.
+				     float scale,
+				     // Additional scale for c2, which can stretch it.
+				     // (we use scale * xscale2, scale * yscale2)
+				     float xscale2, float yscale2,
+				     // Offsets for c2. Maybe depends on scale?
+				     float xmov2, float ymov2);
 
-#endif
-			  
+  // Returns the best CharBitmapDifference for each letter A-Z vs a-z.
+  // Maximum return value is 26, if every letter disagrees on every pixel.
+  // Passing in a threshold returns early once we know we'll exceed that threshold,
+  // saving time.
+  // Increasing bitmap_scale and iters_per_char increase accuracy, but also cost.
+  // Defaults seem to be fine.
+  static double TotalAlphabetDifference(const TTF &ttf,
+					float bitmap_scale = 200.0f,
+					int iters_per_char = 1000,
+					float threshold = 26.0);
 };
 
 
