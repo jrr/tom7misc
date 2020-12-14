0ced425476c0d3962b6581c3a393bb7c253b8848
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2020-12-14 05:36:54 +0000

    Compute PR curve

--- lowercase/ui.cc
+++ lowercase/ui.cc
@@ -239,19 +239,86 @@ struct FontDB {
   }
   
   void Save() {
-    vector<string> lines;
-    // XXX sort by filename
-    for (const auto &[filename, info] : files) {
-      lines.push_back(StringPrintf("%s %.5f %s %s",
-				   FlagString(info.flags).c_str(),
-				   info.bitmap_diffs,
-				   TypeString(info.t),
-				   filename.c_str()));
+    {
+      vector<string> lines;
+      // XXX sort by filename
+      for (const auto &[filename, info] : files) {
+	lines.push_back(StringPrintf("%s %.5f %s %s",
+				     FlagString(info.flags).c_str(),
+				     info.bitmap_diffs,
+				     TypeString(info.t),
+				     filename.c_str()));
+      }
+      Util::WriteLinesToFile(DATABASE_FILENAME, lines);
+      printf("Wrote %lld entries to %s\n",
+	     (int64)lines.size(),
+	     DATABASE_FILENAME);
     }
-    Util::WriteLinesToFile(DATABASE_FILENAME, lines);
-    printf("Wrote %lld entries to %s\n",
-	   (int64)lines.size(),
-	   DATABASE_FILENAME);
+      
+    {
+      // Temporary? P/R curve export
+      struct Labeled {
+	// "0" = more likely to be same case, 1 = least likely.
+	float score = 0.0;
+	bool same_case = false;
+	Labeled(float score, bool same_case) :
+	  score(score), same_case(same_case) {}
+      };
+
+      vector<Labeled> labs;
+      for (const auto &[filename, info] : files) {
+	if (info.bitmap_diffs >= 0.0 && info.bitmap_diffs <= 1.0) {
+	  auto it = info.flags.find(Flag::SAME_CASE);
+	  if (it != info.flags.end()) {
+	    labs.emplace_back(info.bitmap_diffs, it->second);
+	  }
+	}
+      }
+
+      std::sort(labs.begin(), labs.end(),
+		[](const Labeled &a, const Labeled &b) {
+		  return a.score < b.score;
+		});
+      
+      // "Positive" here means same case (this is a low score).
+      //
+      // As we go, assuming the threshold is set to the current value, what would
+      // our P/R be? This means calling everything we've already seen a positive
+      // and everything else a negative. So first, a parallel array giving the
+      // number of true positives for the rest of the array (strictly higher scores).
+      vector<int64> remaining_positives(labs.size(), 0);
+      int64 total_positives = 0;
+      {
+	int64 pos_above = 0;
+	for (int64 i = labs.size() - 1; i >= 0; i--) {
+	  remaining_positives[i] = pos_above;
+	  if (labs[i].same_case) {
+	    total_positives++;
+	    pos_above++;
+	  }
+	}
+      }
+
+      // Now compute precision at each threshold. Every item up to the threshold
+      // being considered is predicted positive.
+      int64 positives_so_far = 0;
+      std::vector<string> lines;
+      lines.reserve(labs.size() + 1);
+      lines.push_back("threshold\t"
+		      "recall\t"
+		      "precision");
+      for (int64 i = 0; i < labs.size(); i++) {
+	if (labs[i].same_case) positives_so_far++;
+	double precision = (double)positives_so_far / (i + 1);
+	lines.push_back(StringPrintf("%.5f\t%.5f\t%.5f",
+				     labs[i].score,
+				     (total_positives - remaining_positives[i]) /
+				     (double)total_positives,
+				     precision));
+      }
+      Util::WriteLinesToFile("pr-curve.tsv", lines);
+    }
+
     dirty = false;
   }
 
@@ -400,8 +467,10 @@ double BitmapDifference(const TTF &ttf,
   const float subpixel_y = ymov2 - int_y;
 #endif
 
-  printf ("scale %.5f %.5f\n",
-      stb_scale * xscale2, stb_scale * yscale2);
+  if (draw) {
+    printf ("scale %.5f %.5f\n",
+	    stb_scale * xscale2, stb_scale * yscale2);
+  }
   
   int width2, height2, xoff2, yoff2;
   uint8 *bit2 = stbtt_GetCodepointBitmapSubpixel(info,
@@ -1010,7 +1079,9 @@ void UI::DrawSortition() {
       string dest = "";
       string cstring = "";
       if (info.has_value()) {
-	dest = TypeString(info.value().t);
+	if (info.value().t != Type::UNKNOWN) {
+	  dest = TypeString(info.value().t);
+	}
 	const auto &flags = info.value().flags;
 	auto it = flags.find(Flag::SAME_CASE);
 	if (it != flags.end()) {
