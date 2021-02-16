79bac91d3917f6f795e7b8e88ea7d0d7ac52b7b8
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2021-02-16 00:11:53 +0000

    Tried doing >0 instead of EVEN_ODD but it turns out this is not easy with SDF. Probably not that interesting anyway

--- trunk/lowercase/hallucinate.cc
+++ trunk/lowercase/hallucinate.cc
@@ -1,4 +1,8 @@
 
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -12,6 +16,9 @@
 #include "image.h"
 #include "threadutil.h"
 #include "base/stringprintf.h"
+#include "lines.h"
+
+
 
 using namespace std;
 
@@ -23,7 +30,7 @@ using int64 = int64_t;
 // for the target letter, and minimizes others.
 // Otherwise find the "perfect input" that is closest
 // to the vector [0, 0, ... 0, 1, 0, ... 0].
-static constexpr bool FIND_MAX = true;
+static constexpr bool FIND_MAX = false;
 
 
 static constexpr FontProblem::SDFConfig SDF_CONFIG = {};
@@ -367,20 +374,239 @@ static ImageA DrawBitmapShapes(const Network &net,
 }
 
 
-int main(int argc, char **argv) {
-  // XXX make command-line option
-  std::unique_ptr<Network> net;
-  net.reset(Network::ReadNetworkBinary("net0.val"));
+// Generate an SDF by generating the SDF directly from
+// some parameterized shapes. Should be equivalent to the
+// above but just faster.
+static ImageA DrawSDFShapes(const Network &net,
+                            int target_letter) {
+  constexpr int sdf_size = SDF_CONFIG.sdf_size;
+  constexpr int NUM_TRIANGLES = 0; // 6; // 9;
+  constexpr int NUM_CIRCLES = 0; // 4;
+  constexpr int NUM_BOXES = 2; // 6; // 4;
 
+  // three vertices
+  constexpr int TRIANGLE_SIZE = 3 * 2;
+  // center and radius
+  constexpr int CIRCLE_SIZE = 2 + 1;
+  // two corner vertices
+  constexpr int BOX_SIZE = 2 * 2;
+
+  constexpr int TRIANGLE_START = 0;
+  constexpr int CIRCLE_START = TRIANGLE_START + NUM_TRIANGLES * TRIANGLE_SIZE;
+  constexpr int BOX_START = CIRCLE_START + NUM_CIRCLES * CIRCLE_SIZE;
+
+  constexpr int N = BOX_START + NUM_BOXES * BOX_SIZE;
+
+  // EVEN_ODD works fine, but I think >0 mode needs some other approach...
+  // the interior segments should be ignored for distance sake. It can't
+  // just be an operation over independent SDFs either (imagine two
+  // rectangles that are very slightly overlapping). I think I'd need
+  // to do some CSG stuff before computing this.
+  constexpr bool EVEN_ODD = true;
+
+  // Unlike the above, arguments are in [0, sdf_size].
+  auto MakeSDF = [](const std::vector<double> &inputs) {
+      ImageA sdf(sdf_size, sdf_size);
+
+      // Draw the shapes.
+      for (int y = 0; y < sdf_size; y++) {
+        for (int x = 0; x < sdf_size; x++) {
+
+          // In even-odd mode, the distance to any edge (each one
+          // is a transition.)
+          float min_dist = sdf_size * sdf_size * 2;
+          // In nonzero mode, we compute the signed distance
+          // as we go.
+          float signed_dist = 0.0f;
+          int count_inside = 0;
+          for (int i = 0; i < NUM_TRIANGLES; i++) {
+            float x0 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 0];
+            float y0 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 1];
+            float x1 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 2];
+            float y1 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 3];
+            float x2 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 4];
+            float y2 = inputs[TRIANGLE_START + i * TRIANGLE_SIZE + 5];
+
+            bool inside = PointInside(x0, y0, x1, y1, x2, y2, x, y);
+
+            if (EVEN_ODD) {
+              min_dist = std::min({
+                  PointLineDistance(x0, y0, x1, y1, x, y),
+                  PointLineDistance(x1, y1, x2, y2, x, y),
+                  PointLineDistance(x2, y2, x0, y0, x, y),
+                  min_dist});
+              if (inside) count_inside++;
+            } else {
+              // inside any?
+              if (inside) {
+                // We're inside so distance will be non-negative.
+                signed_dist = std::max({
+                  PointLineDistance(x0, y0, x1, y1, x, y),
+                  PointLineDistance(x1, y1, x2, y2, x, y),
+                  PointLineDistance(x2, y2, x0, y0, x, y),
+                  signed_dist});
+              } else {
+                // Otherwise, non-positive.
+                signed_dist = std::min({
+                    -PointLineDistance(x0, y0, x1, y1, x, y),
+                    -PointLineDistance(x1, y1, x2, y2, x, y),
+                    -PointLineDistance(x2, y2, x0, y0, x, y),
+                    -signed_dist});
+              }
+            }
+          }
+
+          for (int i = 0; i < NUM_CIRCLES; i++) {
+            float cx = inputs[CIRCLE_START + i * CIRCLE_SIZE + 0];
+            float cy = inputs[CIRCLE_START + i * CIRCLE_SIZE + 1];
+            // Could precompute the squaring here, or just say
+            // the radius is squared to begin with.
+            float r = inputs[CIRCLE_START + i * CIRCLE_SIZE + 2];
+
+            float dx = x - cx;
+            float dy = y - cy;
+            float dist = sqrtf(dx * dx + dy * dy);
+
+            bool inside = dist < r;
+
+            if (EVEN_ODD) {
+              min_dist = std::min({
+                  fabs(dist - r),
+                  min_dist});
+              if (inside) count_inside++;
+            } else {
+              if (inside) {
+                signed_dist = std::max(dist, signed_dist);
+              } else {
+                signed_dist = std::min(-dist, signed_dist);
+              }
+            }
+          }
+
+          for (int i = 0; i < NUM_BOXES; i++) {
+            float x0 = inputs[BOX_START + i * BOX_SIZE + 0];
+            float y0 = inputs[BOX_START + i * BOX_SIZE + 1];
+            float x1 = inputs[BOX_START + i * BOX_SIZE + 2];
+            float y1 = inputs[BOX_START + i * BOX_SIZE + 3];
+
+            // Allow inside-out rectangles.
+            if (x0 > x1) std::swap(x0, x1);
+            if (y0 > y1) std::swap(y0, y1);
+
+            bool inside = (x >= x0 && x <= x1 &&
+                           y >= y0 && y <= y1);
+
+            // PERF: Box distance test could be faster via case
+            // analysis for the 9 regions, although it's plausible
+            // the compiler can simplify redundant tests in here...
+            float dist = std::min({
+                PointHorizLineDistance(x0, y0, x1, x, y),
+                PointVertLineDistance(x1, y0, y1, x, y),
+                PointHorizLineDistance(x1, y1, x0, x, y),
+                PointVertLineDistance(x0, y1, y0, x, y)});
+
+            if (EVEN_ODD) {
+              min_dist = std::min(dist, min_dist);
+              if (inside) count_inside++;
+            } else {
+              if (inside) {
+                signed_dist = std::max(dist, signed_dist);
+              } else {
+                signed_dist = std::min(-dist, signed_dist);
+              }
+            }
+          }
+
+          float pixel_dist = 0.0f;
+          if (EVEN_ODD) {
+            pixel_dist = (0 == (count_inside & 1)) ? -min_dist : min_dist;
+          } else {
+            pixel_dist = signed_dist;
+          }
+
+          float falloff = SDF_CONFIG.falloff_per_pixel * pixel_dist;
+          uint8 v = std::clamp((int)roundf(SDF_CONFIG.onedge_value + falloff),
+                               0, 255);
+          sdf.SetPixel(x, y, v);
+        }
+      }
+
+      return sdf;
+    };
+
+  int num_calls = 0;
+  std::function<double(const std::vector<double> &inputs)> IsLetter =
+    [&net, target_letter, &MakeSDF, &num_calls](
+        const std::vector<double> &inputs) -> double {
+
+      ImageA sdf = MakeSDF(inputs);
+      const auto [out_sdf, pred] =
+        FontProblem::RunSDFModel(net, SDF_CONFIG, sdf);
+
+      double penalty =
+        LetterPenalty(target_letter,
+                      [&pred](int i) {
+                        return pred[i];
+                      });
+
+      num_calls++;
+      if (num_calls % 1000 == 0)
+        printf("[%c] %d calls, penalty: %.4f\n",
+               target_letter + 'a', num_calls, penalty);
+
+      return penalty;
+    };
+
+  // All coordinates in [0,sdf_size].
+  const std::vector<double> lbs(N, 0.0);
+  const std::vector<double> ubs(N, (double)sdf_size);
+  Timer optimize_timer;
+  const auto [best, penalty] =
+    Opt::Minimize(N,
+                  IsLetter,
+                  lbs,
+                  ubs,
+                  // 10000,
+                  // 5000,
+                  100,
+                  1,
+                  10 // 240
+                  );
+  double opt_ms = optimize_timer.MS();
+
+  printf("Result penalty %.5f in %.2f\n", penalty, opt_ms / 1000.0);
+  CHECK(best.size() == N);
+  return MakeSDF(best);
+}
+
+
+
+int main(int argc, char **argv) {
+  #ifdef WIN32
+  if (!SetPriorityClass(GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS)) {
+    LOG(FATAL) << "Unable to go to BELOW_NORMAL priority.\n";
+  }
+  #endif
+
+  // XXX make command-line option
+  const string model_file = "net0.val";
+
+  std::unique_ptr<Network> net;
+  net.reset(Network::ReadNetworkBinary(model_file));
+
+  Timer all_timer;
   std::mutex text_m;
-  string text;
+  string text = StringPrintf("From model %s, find %s\n",
+                             model_file.c_str(),
+                             FIND_MAX ? "max" : "perfect");
 
   ParallelComp(
       26,
       [&net, &text_m, &text](int target_letter) {
         Timer letter_timer;
         // ImageA best_sdf = DirectOptimizeSDF(*net, target_letter);
-        ImageA best_sdf = DrawBitmapShapes(*net, target_letter);
+        // ImageA best_sdf = DrawBitmapShapes(*net, target_letter);
+        ImageA best_sdf = DrawSDFShapes(*net, target_letter);
 
         // Pass same net twice, we'll just use the "lowercase"
         FontProblem::GenResult result =
@@ -388,9 +614,20 @@ int main(int argc, char **argv) {
 
 
         ImageRGBA out(result.input.Width(),
-                      result.input.Height() * 2);
+                      result.input.Height() * 2 + 12);
         out.BlendImage(0, 0, result.input);
         out.BlendImage(0, result.input.Height(), result.low);
+
+        for (int i = 0; i < 26; i++) {
+          const uint8 v = FontProblem::FloatByte(result.low_pred[i]);
+          string s = "A";
+          s[0] += i;
+          out.BlendText(9 * i,
+                        result.input.Height() * + 2,
+                        v, v, 0xFF, 0xFF,
+                        s);
+        }
+
 
         char c = 'a' + target_letter;
 
@@ -414,6 +651,13 @@ int main(int argc, char **argv) {
 
       },
       6);
+  const double all_total_s = (all_timer.MS() / 1000.0);
+  const int all_m = all_total_s / 60.0;
+  const double all_s = fmod(all_total_s, 60.0);
+  string timing = StringPrintf("\nTook %.2f sec = %d m %.2f sec\n",
+                               all_total_s, all_m, all_s);
+  text += timing;
+  printf("%s", timing.c_str());
 
   Util::WriteFile("hallucinated.txt", text);
   return 0;
